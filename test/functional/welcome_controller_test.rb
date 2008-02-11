6e96ca125a31b7e2753a79f62508b08e80f63b1e
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-02-11 18:18:49 +0100

    add test_index

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_index
+    get :index
+    assert_response :success
   end
 end
