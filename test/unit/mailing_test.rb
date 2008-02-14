093b3c9be2f3245229bb24d5b103e0a0e7a55cbe
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-02-14 09:34:51 +0100

    updated mailings fixture, added find test

--- test/unit/mailing_test.rb
+++ test/unit/mailing_test.rb
@@ -1,8 +1,10 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class MailingTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :mailings
+  
+  def test_find
+    assert_equal mailings(:unprinted)["doctor_id"], 1
+    assert_equal mailings(:printed)["doctor_id"], 1
   end
 end
