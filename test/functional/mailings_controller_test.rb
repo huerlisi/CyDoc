3cc12734ff5d75ca6717026eb69cade78f224c67
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-02-14 15:59:01 +0100

    add tests for mailings list

--- test/functional/mailings_controller_test.rb
+++ test/functional/mailings_controller_test.rb
@@ -1,6 +1,8 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class MailingsControllerTest < ActionController::TestCase
+  fixtures :mailings
+  
   def test_index
     get :index
     assert_response :redirect
@@ -9,6 +11,17 @@ class MailingsControllerTest < ActionController::TestCase
   def test_list
     get :list
     assert_response :success
+
+    assert_tag :content => 'Alle Resultate drucken', :parent => :a
+    assert_tag :content => 'Nur Übersicht drucken', :parent => :a
+
+    assert_tag :content => '0', :parent => { 
+      :tag => :a, :attributes => { 
+        :href => @controller.url_for(:controller => :mailings, :action => :show, :id => 1)
+      }
+    }
+
+    assert_tag :content => /Unbekannter Arzt/
   end
 
   def test_overview
