e3e716b6d187aebfce50ba0ba9e13b2224f98663
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-06-30 13:08:09 +0200

    adapt finding_classes from hozr

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,3 +1,17 @@
 class Case < ActiveRecord::Base
   belongs_to :classification
+  has_and_belongs_to_many :finding_classes
+  belongs_to :patient
+
+  def control_findings
+    finding_classes.select { |finding| finding.belongs_to_group?('Kontrolle') }
+  end
+
+  def quality_findings
+    finding_classes.select { |finding| finding.belongs_to_group?('Zustand') }
+  end
+
+  def findings
+    finding_classes.select { |finding| !(finding.belongs_to_group?('Zustand') || finding.belongs_to_group?('Kontrolle'))}
+  end
 end
