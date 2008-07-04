bad138e80a9c56f19343ec5ba0e6c17f754f1fff
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-04 13:06:23 +0200

    set vcards, doctor, insurance params in patients/show

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,5 +2,9 @@ class PatientsController < ApplicationController
 
   def show
     @patient = Patient.find(params[:id])
+    @vcard = @patient.vcard
+    @billing_vcard = @patient.billing_vcard
+    @insurance = @patient.insurance
+    @doctor = @patient.doctor
   end
 end
