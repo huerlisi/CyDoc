b7d9e0c8dec8d6986484b79c7bc1c98c55a97ca9
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-09 15:46:58 +0200

    more layouting and simplification

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -1,6 +1,2 @@
 module CasesHelper
-  def finding_css_class(finding)
-    css_class = "finding_class_#{finding.code} "
-    css_class += finding.finding_groups.collect { |group| "finding_group_#{group.name}" }.join(' ')
-  end
 end
