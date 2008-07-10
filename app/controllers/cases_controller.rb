a63a89621e7190309173860a7e448e3aa97780ec
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-10 12:03:45 +0200

    use send_file for order_form image, makes it possible to check for authorization

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -8,4 +8,9 @@ class CasesController < ApplicationController
     show
     render :action => :show
   end
+
+  def order_form
+    @case = Case.find(params[:id])
+    send_file(@case.order_form.file('result_remarks'), :type => 'image/jpeg', :disposition => 'inline')
+  end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,2 +1,14 @@
 class OrderForm < ActiveRecord::Base
+  file_column :file, :magick => {
+    :versions => {
+      :full => {:size => "550"},
+      :address => {:transformation => Proc.new { |image| image.crop(::Magick::NorthWestGravity, image.rows, image.columns * 0.5, true) }, :size => "560"},
+      :remarks => {:transformation => :extract_remarks },
+      :result_remarks => {:transformation => :extract_result_remarks, :size => "620"},
+      :overview => {:transformation => Proc.new { |image| image.crop(::Magick::NorthWestGravity, 0, 75, 1155, 360, true) } },
+      :head => {:transformation => Proc.new { |image| image.crop(::Magick::NorthWestGravity, 0, 0, 1172, 586, true) }, :size => "500" },
+      :head_big => {:transformation => Proc.new { |image| image.crop(::Magick::NorthWestGravity, 0, 0, 1172, 586, true) }, :size => "750" },
+      :foot => {:transformation => Proc.new { |image| image.crop(::Magick::NorthWestGravity, 0, 1137, 1172, 469, true) }, :size => "500" }
+    }
+  }
 end
