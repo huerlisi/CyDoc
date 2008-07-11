bc290bfd4e3463a01fc6dd077b10c90cdeae967c
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-11 13:31:45 +0200

    implement case actions: get order_form, get pdf(a4)

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -23,8 +23,18 @@ class CasesController < ApplicationController
     render :action => :show
   end
 
-  def order_form
+  def result_remarks
     @case = Case.find(params[:id])
     send_file(@case.order_form.file('result_remarks'), :type => 'image/jpeg', :disposition => 'inline')
+  end
+
+  def order_form
+    @case = Case.find(params[:id])
+    send_file(@case.order_form.file, :type => 'image/jpeg', :disposition => 'inline')
+  end
+
+  def pdf_a4
+    @case = Case.find(params[:id])
+    send_file(File.join(RAILS_ROOT, '/data/result_reports/', "result_report-#{@case.id}.pdf"), :type => 'application/pdf', :disposition => 'inline')
   end
 end
