aadcc7498667f246b735f1f3c077f2a84bd808ee
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-16 13:28:03 +0200

    use current mailing as default result view

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -1,7 +1,7 @@
 class MailingsController < ApplicationController
   # Authorization
   # =============
-  before_filter :authorize, :except => [:list, :index]
+  before_filter :authorize, :except => [:list, :index, :latest_overview]
   private
   def authorize
     begin
@@ -15,16 +15,26 @@ class MailingsController < ApplicationController
   # =======
   public
   def index
-    redirect_to :action => :list
+    redirect_to :action => :latest_overview
   end
   
+  def latest_overview
+    @mailing = Mailing.find(:first, :order => 'printed_at desc', :conditions => ['doctor_id = ?', @current_doctor.id])
+    render :action => 'overview', :layout => 'cases'
+  end
+
   def overview
     @mailing = Mailing.find(params[:id])
-    render :layout => 'cases'
+  end
+
+  def overview_inline
+    overview
+    render :action => 'overview', :layout => false
   end
 
   def list
     @mailings = Mailing.find(:all, :conditions => ['doctor_id = ?', @current_doctor.id])
+    render :layout => 'cases'
   end
 
   def statistics
