9e034bc3c6566e15b99be8bcc661e7c8f7edc9bf
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-20 17:56:49 +0200

    implement creation of new medical_case; enhance medical_case partial dispatcher

--- app/controllers/medical_cases_controller.rb
+++ app/controllers/medical_cases_controller.rb
@@ -1,5 +1,4 @@
 class MedicalCasesController < ApplicationController
-
   def show
   end
 
@@ -13,8 +12,23 @@ class MedicalCasesController < ApplicationController
   end
 
   def new
+    @medical_case = Object.const_get(params[:type]).new
+    @medical_case.doctor = @current_doctor
+    @medical_case.patient_id = params[:patient_id]
   end
 
   def new_inline
+    new
+    render :action => 'new', :layout => false
+  end
+
+  def create
+    @medical_case = Object.const_get(params[:medical_case][:type]).new(params[:medical_case])
+    if @medical_case.save
+      flash[:notice] = 'Erfolgreich erfasst.'
+      redirect_to :controller => 'patients', :action => 'show', :id => @medical_case.patient_id
+    else
+      render :action => 'new'
+    end
   end
 end
