2fd4fa4ec1f663e221bc25e9f07f716cc1b12d13
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-21 14:39:34 +0200

    make _inline actions for tariff_items controller inline

--- app/controllers/tariff_items_controller.rb
+++ app/controllers/tariff_items_controller.rb
@@ -4,17 +4,23 @@ class TariffItemsController < ApplicationController
   end
 
   def new_inline
+    new
+    render :layout => false
   end
 
   def edit
   end
 
   def edit_inline
+    edit
+    render :layout => false
   end
 
   def destroy
   end
 
   def destroy_inline
+    destroy
+    render :layout => false
   end
 end
