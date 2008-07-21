4bc3689e2a034405175da703a32b419d72f59fbf
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-21 14:36:46 +0200

    new controller tariff_items

---
+++ app/controllers/tariff_items_controller.rb
@@ -0,0 +1,20 @@
+class TariffItemsController < ApplicationController
+
+  def new
+  end
+
+  def new_inline
+  end
+
+  def edit
+  end
+
+  def edit_inline
+  end
+
+  def destroy
+  end
+
+  def destroy_inline
+  end
+end
---
+++ app/helpers/tariff_items_helper.rb
@@ -0,0 +1,2 @@
+module TariffItemsHelper
+end
