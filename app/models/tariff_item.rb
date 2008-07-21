b073b355c51948741608e72964bfab557e3b7cd3
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-21 14:32:55 +0200

    new model TariffItem

---
+++ app/models/tariff_item.rb
@@ -0,0 +1,2 @@
+class TariffItem < ActiveRecord::Base
+end
---
+++ db/migrate/006_create_tariff_items.rb
@@ -0,0 +1,17 @@
+class CreateTariffItems < ActiveRecord::Migration
+  def self.up
+    create_table :tariff_items do |t|
+      t.integer :patient_id
+      t.integer :doctor_id
+      t.float :amount
+      t.float :tp_al
+      t.float :tp_tl
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :tariff_items
+  end
+end
