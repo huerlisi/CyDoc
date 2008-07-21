b34b0bbed407f807e490a7c7238e5a7d2f970957
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-21 16:34:19 +0200

    implement record_tarmed creation

--- app/controllers/tariff_items_controller.rb
+++ app/controllers/tariff_items_controller.rb
@@ -1,11 +1,28 @@
 class TariffItemsController < ApplicationController
 
   def new
+    @record_tarmed = RecordTarmed.new
+    @record_tarmed.provider_id = @current_doctor.id
+
+    # Defaults
+    @record_tarmed.date = DateTime.now
+    @record_tarmed.session = 1
+    @record_tarmed.patient_id = params[:patient_id]
   end
 
   def new_inline
     new
     render :action => 'new', :layout => false
+  end
+
+  def create
+    @record_tarmed = RecordTarmed.new(params[:record_tarmed])
+    if @record_tarmed.save
+      flash[:notice] = 'Erfolgreich erfasst.'
+      redirect_to :controller => 'patients', :action => 'show', :id => @record_tarmed.patient_id
+    else
+      render :action => 'new'
+    end
   end
 
   def edit
---
+++ db/migrate/008_add_patient_id_to_record_tarmeds.rb
@@ -0,0 +1,9 @@
+class AddPatientIdToRecordTarmeds < ActiveRecord::Migration
+  def self.up
+    add_column :record_tarmeds, :patient_id, :integer
+  end
+
+  def self.down
+    remove_column :record_tarmeds, :patient_id
+  end
+end
