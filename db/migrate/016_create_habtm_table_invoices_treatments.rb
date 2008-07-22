be68821e524694af2dd0e4b99314e1d4bd7159f4
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-22 17:08:45 +0200

    create habtm tables for invoices

---
+++ db/migrate/016_create_habtm_table_invoices_treatments.rb
@@ -0,0 +1,11 @@
+class CreateHabtmTableInvoicesTreatments < ActiveRecord::Migration
+  def self.up
+    create_table "invoices_treatments", :id => false do |t|
+      t.integer "invoice_id"
+      t.integer "treatment_id"
+    end
+  end
+
+  def self.down
+  end
+end
---
+++ db/migrate/017_create_habtm_table_invoices_record_tarmeds.rb
@@ -0,0 +1,11 @@
+class CreateHabtmTableInvoicesRecordTarmeds < ActiveRecord::Migration
+  def self.up
+    create_table "invoices_record_tarmeds", :id => false do |t|
+      t.integer "invoice_id"
+      t.integer "record_tarmed_id"
+    end
+  end
+
+  def self.down
+  end
+end
