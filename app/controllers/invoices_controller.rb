a7d6732331386e2ade751da892a3bb6622d634a3
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-23 14:04:10 +0200

    implement _for_pdf actions for invoices

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -6,4 +6,15 @@ class InvoicesController < ApplicationController
   def tarmed
     @invoice = Invoice.find(params[:id])
   end
+
+  def tarmed_for_pdf
+    tarmed
+    render :layout => 'simple'
+  end
+
+  def tarmed_rueckforderungsbeleg_for_pdf
+    tarmed_rueckforderungsbeleg
+    render :layout => 'simple'
+  end
+
 end
