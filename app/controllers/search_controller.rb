a5f6cb4a012a4b77500f058d202e7297b2ad3384
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-07-23 21:26:02 +0200

    implement patients search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,11 +5,15 @@ class SearchController < ApplicationController
     when "date"
       value = Date.parse(value)
       condition = "(cases.examination_date = :value) OR (patients.birth_date = :value)"
+      patient_condition = "(patients.birth_date = :value)"
     when "entry_nr"
       condition = "(cases.praxistar_eingangsnr = :value OR cases.praxistar_eingangsnr REGEXP concat('../0*', :value) OR patients.doctor_patient_nr = :value)"
+      patient_condition = "(patients.doctor_patient_nr = :value)"
     when "text"
       condition = "(classifications.name REGEXP :value) OR (cases.finding_text LIKE :value) OR (vcards.given_name LIKE :value) OR (vcards.family_name LIKE :value) OR (vcards.full_name LIKE :value)"
+      patient_condition = "(vcards.given_name LIKE :value) OR (vcards.family_name LIKE :value) OR (vcards.full_name LIKE :value)"
     end
+    @patients = Patient.find(:all, :include => [:vcard ], :conditions => ["(#{patient_condition}) AND patients.doctor_id IN (:doctor_id)", {:value => value, :doctor_id => @current_doctor_ids}], :limit => 100)
     @cases = Case.find(:all, :include => [:classification => [], :patient => [ :vcard ]], :conditions => ["(#{condition}) AND cases.doctor_id IN (:doctor_id)", {:value => value, :doctor_id => @current_doctor_ids}], :limit => 100)
 
     render :layout => false
