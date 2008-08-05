284c47623dd54f383eab9e4287dd19824b771228
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-08-05 14:50:34 +0200

    fix colors of pap cases in sidebar history

---
+++ db/migrate/025_add_code_to_classification_groups.rb
@@ -0,0 +1,9 @@
+class AddCodeToClassificationGroups < ActiveRecord::Migration
+  def self.up
+    add_column :classification_groups, :code, :string
+  end
+
+  def self.down
+    remove_column :classification_groups, :code
+  end
+end
