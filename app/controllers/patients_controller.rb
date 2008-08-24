b69b6aeadb09308564d290d88f24896e9329e66a
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-08-24 22:15:22 +0200

    quite some work to get patient search working for lists

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,28 @@
 class PatientsController < ApplicationController
+  in_place_edit_for :vcard, :family_name
+  in_place_edit_for :vcard, :given_name
+  in_place_edit_for :vcard, :street_address
+  in_place_edit_for :vcard, :postal_code
+  in_place_edit_for :vcard, :locality
+  in_place_edit_for :patient, :birth_date
+  in_place_edit_for :patient, :in_place_doctor_patient_nr
+  in_place_edit_for :patient, :in_place_insurance_nr
+                
+  # CRUD Actions
+  def index
+  end
+  
+  def new
+    patient = params[:patient]
+    @patient = Patient.new(patient)
+    @vcard = Vcards::Vcard.new(params[:vcard])
+    @vcard.honorific_prefix = 'Frau'
+    @vcard.address = Vcards::Address.new
+
+    @patients = []
+    render :action => 'list'
+  end
+
   def show
     @patient = Patient.find(params[:id])
 
@@ -13,6 +37,101 @@ class PatientsController < ApplicationController
     @record_tarmed.patient_id = @patient.id
   end
 
+  # Search
+  # ======
+  def vcard_conditions(vcard_name = 'vcard')
+    vcard_params = params[:vcard] || {}
+    keys = []
+    values = []
+
+    fields = vcard_params.reject { |key, value| value.nil? or value.empty? }
+    fields.each { |key, value|
+      keys.push "#{vcard_name}.#{key} LIKE ?"
+      values.push '%' + value.downcase.gsub(' ', '%') + '%'
+    }
+
+    return !keys.empty? ? [ keys.join(" AND "), *values ] : nil
+  end
+
+  def patient_conditions
+    parameters = params[:patient]
+    keys = []
+    values = []
+
+    unless parameters[:full_name].nil? or parameters[:full_name].empty?
+      keys.push "patient_id = ?"
+      values.push parameters[:full_name].split(' ')[0].strip
+    end
+
+    unless parameters[:doctor_patient_nr].nil? or parameters[:doctor_patient_nr].empty?
+      keys.push "doctor_patient_nr = ?"
+      values.push parameters[:doctor_patient_nr].strip
+    end
+
+    unless parameters[:birth_date].nil? or parameters[:birth_date].empty?
+      if parameters[:birth_date].match /bis/
+        lower_bound, higher_bound = parameters[:birth_date].split('-')
+        if Date.date_only_year?(lower_bound)
+            keys.push "YEAR(birth_date) BETWEEN ? AND ?"
+            values.push Date.expand_year(lower_bound.strip)
+            values.push Date.expand_year(higher_bound.strip)
+        else
+            keys.push "birth_date BETWEEN ? AND ?"
+            values.push Date.parse_europe(lower_bound.strip)
+            values.push Date.parse_europe(higher_bound.strip)
+        end
+      else
+        if Date.date_only_year?(parameters[:birth_date])
+            keys.push "YEAR(birth_date) = ?"
+            values.push Date.expand_year(parameters[:birth_date].strip)
+        else
+            keys.push "birth_date = ? "
+            values.push Date.parse_europe(parameters[:birth_date])
+        end
+      end
+    end
+
+    return !keys.empty? ? [ keys.join(" AND "), *values ] : nil
+  end
+
+
+  def search
+    @case = Cyto::Case.find(params[:case_id]) unless params[:case_id].nil?
+  
+    # If entry_nr is given, take it as the only condition
+    eingangsnr = params[:search][:praxistar_eingangsnr]
+    if !eingangsnr.empty?
+      search_by_eingangsnr
+      return
+    end
+
+    keys = []
+    values = []
+    
+    default_vcard_keys, *default_vcard_values = vcard_conditions('vcards')
+    billing_vcard_keys, *billing_vcard_values = vcard_conditions('billing_vcards_patients')
+    
+    unless default_vcard_keys.nil?
+      keys.push "( ( #{default_vcard_keys} ) OR ( #{billing_vcard_keys} ) )"
+      values.push *default_vcard_values
+      values.push *billing_vcard_values
+    end
+    
+    patient_keys, *patient_values = patient_conditions
+    keys.push patient_keys
+    values.push *patient_values
+    
+    # Build conditions array
+    if keys.compact.empty?
+      @patients = []
+    else
+      conditions = !keys.compact.empty? ? [  keys.compact.join(" AND "), *values ] : nil
+      @patients = Patient.find :all, :conditions => conditions, :include => [:vcard, :billing_vcard], :order => 'vcards.family_name'
+    end
+  
+    render :partial => 'search_result'
+  end
+  
   # Services
   def list_services
     @patient = Patient.find(params[:id])
