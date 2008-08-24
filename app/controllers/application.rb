809eb47eb793149c422361df03cd50d32472467b
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-08-24 12:34:48 +0200

    add hack to use REGEXP in SQLite based dbs

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -19,11 +19,30 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |user_name, password|
       @current_doctor = Doctor.find_by_login_and_password(user_name, Digest::SHA256.hexdigest(password))
       if @current_doctor.nil?
+        # Access not granted, return false
         false
       else
         @current_doctor_ids = @current_doctor.colleagues.map{|c| c.id}.uniq
 
         @printers = @current_doctor.office.printers if @current_doctor.office
+
+        # TODO: Nice hack to add REGEXP support for SQLite, but application controller is bad place...
+        if ActiveRecord::Base.connection.adapter_name.eql?("SQLite")
+          db = ActiveRecord::Base.connection.instance_variable_get(:@connection)
+          db.create_function("regexp", 2) do |func, expr, value|
+            begin
+              if value.to_s && value.to_s.match(Regexp.new(expr.to_s))
+                func.set_result 1
+              else
+               func.set_result 0
+              end
+            rescue => e
+              puts "error: #{e}"
+            end
+          end
+        end
+
+        # Return true as access is granted
         true
       end
     end
