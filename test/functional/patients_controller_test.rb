f99de007305fc8b0d55c67d0987b3f94c4daf836
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-08-28 14:59:14 +0200

    don't fail on patient search if patient has no vcards

--- test/functional/patients_controller_test.rb
+++ test/functional/patients_controller_test.rb
@@ -1,8 +1,23 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PatientsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_new
+    get :new
+    assert_response :success
+  end
+
+  def test_search
+    # No match
+    xml_http_request :post, :search, {:patient => {:birth_date => '1.1.2000'}}
+    assert_response :success
+    assert_select ".search_result", false
+
+    # Single match
+    patient = patients(:joe)
+
+    xml_http_request :post, :search, {:patient => {:birth_date => '3.2.1990'}}
+    assert_response :success
+    assert_select ".search_result", 1
+    assert_select "#search_result_patient_#{patient.id}"
   end
 end
