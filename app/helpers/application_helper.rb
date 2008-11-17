4ba87455f06104e386b5836d96114d80ebdcc3b0
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-11-17 15:26:46 +0100

    y

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Navigation
+  def navigation_section(section_title, items = {}, image = "#{title}.png")
+    items = items.map {|title, item|
+      item = {:controller => item} if item.is_a?(Symbol)
+      {:title => title, :url => item}
+    }
+
+    render :partial => 'shared/navigation_section', :locals => {:section_title => section_title, :items => items, :image => image}
+  end
 end
