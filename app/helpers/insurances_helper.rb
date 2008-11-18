64b37516f20c6679f9a5c75fc598194bba9d68b7
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-11-18 14:09:44 +0100

    add insurances controller

---
+++ app/helpers/insurances_helper.rb
@@ -0,0 +1,2 @@
+module InsurancesHelper
+end
---
+++ test/functional/insurances_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class InsurancesControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
