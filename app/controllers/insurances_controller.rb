7e4ff52ef3d0b8821963dd929ebc5f81f2e2acc0
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-11-18 15:53:49 +0100

    implement crud for insurances

--- app/controllers/insurances_controller.rb
+++ app/controllers/insurances_controller.rb
@@ -1,2 +1,25 @@
 class InsurancesController < ApplicationController
+  # CRUD Actions
+  def index
+    redirect_to :action => :list
+  end
+  
+  def list
+    query = params[:query]
+    query ||= params[:search][:query] if params[:search]
+
+    @insurances = Insurance.all
+  end
+
+  def search
+    query = params[:query] || params[:search][:query]
+    query ||= params[:search][:query] if params[:search]
+    @insurances = Insurance.by_name("%#{query}%")
+
+    render :partial => 'list', :layout => false
+  end
+
+  def show
+    @insurance = Insurance.find(params[:id])
+  end
 end
--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -1,3 +1,4 @@
 class Insurance < ActiveRecord::Base
   belongs_to :vcard, :class_name => 'Vcards::Vcard', :foreign_key => 'vcard_id'
+  named_scope :by_name, lambda {|name| {:select => '*, insurances.id', :joins => :vcard, :conditions => Vcards::Vcard.by_name_conditions(name)}}
 end
