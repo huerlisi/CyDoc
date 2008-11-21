33fa662699483edac091f4f299f180d18d5f762e
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-11-21 14:18:45 +0100

    implement TariffItemGroup readonly CRUD

--- app/controllers/tariff_item_groups_controller.rb
+++ app/controllers/tariff_item_groups_controller.rb
@@ -1,2 +1,25 @@
 class TariffItemGroupsController < ApplicationController
+  # CRUD Actions
+  def index
+    redirect_to :action => :list
+  end
+  
+  def list
+    query = params[:query]
+    query ||= params[:search][:query] if params[:search]
+
+    @tariff_item_groups = TariffItemGroup.find :all, :order => 'name'
+  end
+
+  def search
+    query = params[:query] || params[:search][:query]
+    query ||= params[:search][:query] if params[:search]
+    @tariff_item_groups = TariffItemGroup.find(:all, :conditions => ['name LIKE ?', "%#{query}%"])
+
+    render :partial => 'list', :layout => false
+  end
+
+  def show
+    @tariff_item_groups = TariffItemGroup.find(params[:id])
+  end
 end
--- db/migrate/20081121123000_create_tariff_item_groups.rb
+++ db/migrate/20081121123000_create_tariff_item_groups.rb
@@ -1,6 +1,8 @@
 class CreateTariffItemGroups < ActiveRecord::Migration
   def self.up
     create_table :tariff_item_groups do |t|
+      t.string :code
+      t.string :name
 
       t.timestamps
     end
