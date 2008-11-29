35361e76787a94b61cd55122f8ee28f2e1877b08
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-11-29 16:26:49 +0100

    move some logic from record_tarmed relating to tax points to tariff_record

--- app/models/tariff_item.rb
+++ app/models/tariff_item.rb
@@ -1,3 +1,26 @@
 class TariffItem < ActiveRecord::Base
   has_and_belongs_to_many :tariff_item_groups
+
+  # "Constant" fields
+  def unit_mt
+    1.89
+  end
+
+  def unit_tt
+    1.89
+  end
+
+  def unit_factor_mt
+    1.0
+  end
+
+  def unit_factor_tt
+    1.0
+  end
+
+  # Calculated field
+  def amount
+    (self.amount_mt * self.unit_factor_mt * self.unit_mt) + (self.amount_tt * self.unit_factor_tt * self.unit_tt)
+  end
+
 end
