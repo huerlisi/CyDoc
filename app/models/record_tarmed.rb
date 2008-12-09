c77f9436000e90cfb723212becb6d6df36fba273
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-12-09 15:49:37 +0100

    save tariff_type in service_record

--- app/models/record_tarmed.rb
+++ app/models/record_tarmed.rb
@@ -24,6 +24,7 @@ class RecordTarmed < ActiveRecord::Base
     self.amount_tt = leistung.amount_tt || 0.0
     self.unit_factor_tt = leistung.unit_factor_tt
     self.remark = leistung.remark
+    self.tariff_type = leistung.tariff_type
     
     self.unit_mt = leistung.unit_mt
     self.unit_tt = leistung.unit_tt
