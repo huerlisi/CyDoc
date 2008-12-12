07880c033bc2d364679e58c2552bdda0728192c1
Simon Huerlimann <simon.huerlimann@cyt.ch>
2008-12-12 13:02:12 +0100

    support TariffGroups as special case of TariffItem

---
+++ db/migrate/20081212100901_tariff_item_group_is_a_tariff_item.rb
@@ -0,0 +1,11 @@
+class TariffItemGroupIsATariffItem < ActiveRecord::Migration
+  def self.up
+    rename_table :tariff_item_groups_tariff_items, :tariff_items_tariff_items
+
+    drop_table :tariff_item_groups
+  end
+
+  def self.down
+    rename_table :tariff_items_tariff_items, :tariff_item_groups_tariff_items
+  end
+end
