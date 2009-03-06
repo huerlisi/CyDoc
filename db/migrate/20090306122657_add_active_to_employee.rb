e3d965ce92f70683c37cc6d84f57dfdf9febf502
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-03-06 13:47:08 +0100

    First version of Praxistar Employee importer.

---
+++ db/migrate/20090306122657_add_active_to_employee.rb
@@ -0,0 +1,9 @@
+class AddActiveToEmployee < ActiveRecord::Migration
+  def self.up
+    add_column :employees, :active, :boolean, :default => true
+  end
+
+  def self.down
+    remove_column :employees, :active
+  end
+end
