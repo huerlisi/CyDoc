5dfa82ee69cb26ee49d95e68ecf2fcdd1879054c
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-03-10 21:53:56 +0100

    Use polymorphic user association.

---
+++ db/migrate/20090306124939_create_polymorphic_user_association.rb
@@ -0,0 +1,15 @@
+class CreatePolymorphicUserAssociation < ActiveRecord::Migration
+  def self.up
+    add_column :users, :object_id, :integer
+    add_column :users, :object_type, :string
+
+    remove_column :employees, :user_id
+  end
+
+  def self.down
+    remove_column :users, :object_id
+    remove_column :users, :object_type
+
+    add_column :users, :user_id, :integer
+  end
+end
