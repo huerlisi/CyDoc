948576d1da92097d85fbb6a35efe048ebc629eb4
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-03-20 09:25:06 +0100

    Don't use foreign key, but :as for polymorphic user association

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,4 @@
 class Employee < ActiveRecord::Base
   has_one :vcard, :class_name => 'Vcards::Vcard', :foreign_key => 'object_id'
-  has_one :user, :foreign_key => 'object_id'
+  has_one :user, :as => :object
 end
