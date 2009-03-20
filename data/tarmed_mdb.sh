3e369adc4ebfbc343d93ee261e83beffaddfc9a6
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-03-20 15:23:18 +0100

    Add tarmed_mdb.sh from Elexis.

---
+++ data/tarmed_mdb.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Convert an MS Access database file to a PostgreSQL script file.
+# This script uses the MDB Tools (http://mdbtools.sourceforge.net)
+# Copyright (c) 2006, Daniel Lutz and Elexis
+# $Id: mdb2sql 2386 2007-05-18 12:26:04Z danlutz $
+
+TYPE=postgresql
+
+function usage {
+  echo "Usage: $0 [{--postgresql | --mysql | --help}] <MS Access File>"
+  echo
+  echo "This script converts an MS Access database file to PostgreSQL or MySQL commands,"
+  echo "which then can be fed to the psql(1) or mysql(1) tools. The commands are written"
+  echo "to standard output."
+}
+
+# make required changes for PostgreSQL
+function convert_postgresql {
+  cat
+}
+
+# make required changes for MySQL
+function convert_mysql {
+  cat | sed -e 's/DROP TABLE/DROP TABLE IF EXISTS/' | sed -e 's/^--/# --/'
+}
+
+# handle parameters
+
+param=$1
+
+if [ "$param" = "--postgresql" ]; then
+  TYPE=postgresql
+  shift
+elif [ "$param" = "--mysql" ]; then
+  TYPE=mysql
+  shift
+fi
+
+param=$1
+
+if [ "$param" = "--help" ]; then
+  usage
+  exit 0
+fi
+
+DB=$param
+
+if [ -z "$DB" ]; then
+  usage
+  exit 1
+fi
+
+# extract schema
+
+if [ "$TYPE" = "postgresql" ]; then
+  mdb-schema $DB postgres | sed -e 's/Postgres_Unknown 0x0c/text/g' | sed -e 's/Char/varchar/g' | convert_postgresql
+elif [ "$TYPE" = "mysql" ]; then
+  mdb-schema $DB postgres | sed -e 's/Postgres_Unknown 0x0c/text/g' | sed -e 's/Char/varchar/g' | convert_mysql
+fi
+
+# extract data
+
+for f in `mdb-tables $DB`; do mdb-export -q "'" -I -D '%Y-%m-%d %H:%M:%S' $DB $f; done | sed -e 's/)$/);/'
+
