1b90be1d2fe804f44ef7d201622e28b668ab9f60
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-03-24 23:07:16 +0100

    Use special style for patient overview tab handler.

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,7 +1,7 @@
 # Based on redMine code
 module PatientsHelper
   def patient_tabs(patient)
-    tabs = [{:name => 'info', :partial => 'patients/show', :label => patient.to_s},
+    tabs = [{:name => 'overview', :partial => 'patients/show', :label => patient.to_s},
             {:name => 'history', :partial => 'patients/medical_history', :label => 'Krankengeschichte'},
             {:name => 'services', :partial => 'patients/service_list', :label => 'Leistungen'},
             {:name => 'invoices', :partial => 'patients/invoice_tab', :label => 'Rechnungen'},
