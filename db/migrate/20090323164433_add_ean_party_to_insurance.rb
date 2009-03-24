06558ea590febf23869c86858e4bd52d379871b5
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-03-24 13:13:23 +0100

    Add EAN party to insurance schema.

---
+++ db/migrate/20090323164433_add_ean_party_to_insurance.rb
@@ -0,0 +1,9 @@
+class AddEanPartyToInsurance < ActiveRecord::Migration
+  def self.up
+    add_column :insurances, :ean_party, :string, :limit => 13
+  end
+
+  def self.down
+    remove_column :insurances, :ean_party
+  end
+end
