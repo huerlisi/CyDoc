ff84d64418334f9f54efe9ff659a0712d419285f
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-03-24 14:37:00 +0100

    Use polymorphic has_one instead of belongs_to for Insurance.

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -1,6 +1,6 @@
 class Insurance < ActiveRecord::Base
-  belongs_to :vcard, :class_name => 'Vcards::Vcard', :foreign_key => 'vcard_id'
-  named_scope :by_name, lambda {|name| {:select => '*, insurances.id', :joins => :vcard, :order => 'full_name', :conditions => Vcards::Vcard.by_name_conditions(name)}}
+  has_one :vcard, :class_name => 'Vcards::Vcard', :as => 'object'
+  named_scope :by_name, lambda {|name| {:include => :vcard, :order => 'full_name', :conditions => Vcards::Vcard.by_name_conditions(name)}}
 
   def to_s
     [vcard.full_name, vcard.locality].compact.join(', ')
---
+++ db/migrate/20090324133215_remove_vcard_id_from_insurance.rb
@@ -0,0 +1,8 @@
+class RemoveVcardIdFromInsurance < ActiveRecord::Migration
+  def self.up
+    remove_column :insurances, :vcard_id
+  end
+
+  def self.down
+  end
+end
