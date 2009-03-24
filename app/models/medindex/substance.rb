ce67d41cfbb52250c153ae68421195dcd9a7ef9a
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-03-24 22:15:58 +0100

    Start of Medindex Substance importer.

--- app/models/medindex/substance.rb
+++ app/models/medindex/substance.rb
@@ -1,5 +1,17 @@
 module Medindex
   class Substance < Base
+    def self.import
+      for ext_record in self.all
+        int_record = Kernel::DrugSubstance.new
+        
+        int_record.id = ext_record.field('SUBNO')
+        int_record.name = ext_record.field('NAMD')
+
+        puts int_record.to_s
+        
+        int_record.save!
+      end
+    end
   end
 end
 
