1d421dafeecbf4ad983050154878324ccbffa7af
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-03-27 09:13:53 +0100

    Create Drug Substance migration.

---
+++ db/migrate/20090324144601_create_drug_substances.rb
@@ -0,0 +1,15 @@
+class CreateDrugSubstances < ActiveRecord::Migration
+  def self.up
+    create_table :drug_substances, :id => false do |t|
+      t.string :id
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :drug_substances
+  end
+end
