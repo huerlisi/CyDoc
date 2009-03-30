8974485ba0eaba7db43e3e3ac3a3c9bc905aac03
root <root@praxis-bonescu.(none)>
2009-03-30 13:38:20 +0200

    Drop unneeded migration add_type_to_tariff_items.

--- db/migrate/20081129152818_add_type_to_tariff_items.rb
+++
@@ -1,9 +0,0 @@
-class AddTypeToTariffItems < ActiveRecord::Migration
-  def self.up
-    add_column :tariff_items, :type, :string
-  end
-
-  def self.down
-    remove_column :tariff_items, :type
-  end
-end
