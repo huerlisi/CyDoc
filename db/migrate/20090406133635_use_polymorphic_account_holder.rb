b5c334ef5ae748b5d826940e7c3a8a5e0d5f1b3d
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-06 15:52:42 +0200

    Use polymorphic account holder.

---
+++ db/migrate/20090406133635_use_polymorphic_account_holder.rb
@@ -0,0 +1,19 @@
+class UsePolymorphicAccountHolder < ActiveRecord::Migration
+  def self.up
+    add_column :accounts, :holder_id, :integer
+    add_column :accounts, :holder_type, :string
+    
+    for bank_account in Accounting::BankAccount.all
+      next if bank_account.holder_vcard_id.nil?
+      
+      vcard = Vcards::Vcard.find(bank_account.holder_vcard_id)
+      bank_account.holder = vcard.object
+      bank_account.save
+    end
+
+    remove_column :accounts, :holder_vcard_id
+  end
+
+  def self.down
+  end
+end
