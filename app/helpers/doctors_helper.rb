538734c1d404f4f7cb77c67ef8824cc826b64526
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-11 12:33:09 +0200

    Implement some doctor tabs.

--- app/helpers/doctors_helper.rb
+++ app/helpers/doctors_helper.rb
@@ -1,2 +1,9 @@
 module DoctorsHelper
+  def doctor_tabs(doctor)
+    tabs = [{:name => 'overview', :partial => 'doctors/show', :label => doctor.to_s},
+            {:name => 'accounting', :partial => 'doctors/accounting_tab', :label => 'Konten'},
+#            {:name => 'printing', :partial => 'doctors/printing_tab', :label => 'Drucker Einstellungen'},
+#            {:name => 'billing', :partial => 'doctors/billing_tab', :label => 'Rechnungs Einstellungen'},
+            ]
+  end
 end
