b60dce36712114f0abf8301a7cd615a567a23aa2
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-12 17:24:20 +0200

    Generalize LabTariffItem.import.

--- app/models/analyseliste/lab_tariff_item.rb
+++ app/models/analyseliste/lab_tariff_item.rb
@@ -1,8 +1,10 @@
 module Analyseliste
   class LabTariffItem < Base
     def self.import
+      int_class = ("Kernel::" + self.name.demodulize).constantize
+      
       for ext_record in self.all
-        int_record = Kernel::LabTariffItem.new(
+        int_record = int_class.new(
 		:code => ext_record[2],
 		:amount_tt => ext_record[4],
 		:remark => ext_record[5]
