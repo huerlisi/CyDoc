bfc375c99c90a104b744f47063bfe52211ad1b27
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-12 17:40:10 +0200

    Don't bail out on import error; show some stats.

--- app/models/analyseliste/lab_tariff_item.rb
+++ app/models/analyseliste/lab_tariff_item.rb
@@ -3,17 +3,28 @@ module Analyseliste
     def self.import
       int_class = ("Kernel::" + self.name.demodulize).constantize
       
+      success = 0
+      errors = 0
       for ext_record in self.all
-        int_record = int_class.new(
-		:code => ext_record[2],
-		:amount_tt => ext_record[4],
-		:remark => ext_record[5]
-        )
+        begin
+          int_record = int_class.new(
+                  :code => ext_record[2],
+                  :amount_tt => ext_record[4],
+                  :remark => ext_record[5]
+          )
         
-        puts int_record.to_s
-        
-        int_record.save!
+          puts int_record.to_s
+
+          int_record.save!
+          success += 1
+        rescue
+          errors += 1
+        end
       end
+      
+      puts
+      puts "Success: #{success}, errors: #{errors}"
+      puts "Import done."
     end
   end
 end
