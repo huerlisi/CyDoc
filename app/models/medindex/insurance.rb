648d94d03d0ff13df18e58060a875abe695ce189
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-12 17:42:48 +0200

    Use same design for Medindex::Insurance.import as Analyseliste::LabTariffItem.import

--- app/models/medindex/insurance.rb
+++ app/models/medindex/insurance.rb
@@ -3,22 +3,33 @@ module Medindex
     def self.import
       int_class = ("Kernel::" + self.name.demodulize).constantize
 
+      success = 0
+      errors = 0
       for ext_record in self.all
-        int_record = int_class.new
-        
-        int_record.ean_party = ext_record.field('EAN')
-        int_record.vcard = Vcards::Vcard.new(
-        	:full_name => ext_record.field('DESCR1'),
-        	:street_address => [ext_record.field('ADDR/STREET'), ext_record.field('ADDR/STRNO')].compact.join(" "),
-              	:extended_address => ext_record.field('ADDR/POBOX'),
-              	:postal_code => ext_record.field('ADDR/ZIP'),
-        	:locality => ext_record.field('ADDR/CITY')
-        )
+        begin
+          int_record = int_class.new
+          
+          int_record.ean_party = ext_record.field('EAN')
+          int_record.vcard = Vcards::Vcard.new(
+                  :full_name => ext_record.field('DESCR1'),
+                  :street_address => [ext_record.field('ADDR/STREET'), ext_record.field('ADDR/STRNO')].compact.join(" "),
+                  :extended_address => ext_record.field('ADDR/POBOX'),
+                  :postal_code => ext_record.field('ADDR/ZIP'),
+                  :locality => ext_record.field('ADDR/CITY')
+          )
 
-        puts int_record.ean_party
-        
-        int_record.save!
+          puts int_record.to_s
+          
+          int_record.save!
+          success += 1
+        rescue
+          errors += 1
+        end
       end
+      
+      puts
+      puts "Success: #{success}, errors: #{errors}"
+      puts "Import done."
     end
   end
 end
