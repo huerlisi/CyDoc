400151353411458d831e734586facf8218b339fd
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-12 21:25:09 +0200

    Fix and comment generic importer.

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -24,27 +24,35 @@ class Importer
   def self.import(do_clean = false)
     puts "Importing #{self.name}..."
 
+    # Clear all entries if demanded
     if do_clean
       puts "  Deleting all #{int_class.count} records..."
       int_class.delete_all
       puts "  Done."
     end
 
+    # Prepare counter variables
     success = 0
     skipped = 0
     errors = 0
 
+    # Assure our data is loaded
+    self.load
     ext_records = self.all
     
     puts "  Importing #{ext_records.count} records..."
-    for ext_record in self.all
+    for ext_record in ext_records
       begin
+        # Do the actual import as specified by sub-classes
         int_record = self.import_record(ext_record)
 
         puts "    " + int_record.to_s
         
+        # Save new record
         int_record.save!
         success += 1
+
+      # If something goes wrong...
       rescue Exception => ex
         puts "Error: #{ex.message}"
         
