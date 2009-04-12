d6248e1648c098bf75b779c5d0364f50668e390d
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-12 19:33:30 +0200

    Moved import function into Medindex::Base; make Insurance use it.

--- app/models/medindex/base.rb
+++ app/models/medindex/base.rb
@@ -15,8 +15,48 @@ module Medindex
       xml.root.elements
     end
 
-    def self.import
-      Medindex::Insurance.import
+    def self.int_class
+      ("Kernel::" + self.name.demodulize).constantize
+    end
+
+    def self.import(do_clean = false)
+      puts "Importing #{self.name}..."
+
+      if do_clean
+        puts "  Deleting all #{int_class.count} records..."
+        int_class.delete_all
+        puts "  Done."
+      end
+
+      success = 0
+      skipped = 0
+      errors = 0
+
+      ext_records = self.all
+      
+      puts "  Importing #{ext_records.count} records..."
+      for ext_record in self.all
+        begin
+          int_record = self.import_record(ext_record)
+
+          puts "    " + int_record.to_s
+          
+          int_record.save!
+          success += 1
+        rescue Exception => ex
+          puts "Error: #{ex.message}"
+          
+          errors += 1
+        end
+      end
+      
+      puts
+      puts "  Success: #{success}; skipped: #{skipped}; errors: #{errors}"
+      puts "Import done."
+    end
+
+    def self.import_all(do_clean = false)
+      Medindex::Insurance.import(do_clean)
     end
   end
 end
--- app/models/medindex/insurance.rb
+++ app/models/medindex/insurance.rb
@@ -1,48 +1,18 @@
 module Medindex
   class Insurance < Base
-    def self.import(do_clean = true)
-      puts "Importing #{self.name}..."
-
-      int_class = ("Kernel::" + self.name.demodulize).constantize
-
-      if do_clean
-        puts "  Deleting all #{int_class.count} records..."
-        int_class.delete_all
-        puts "  Done."
-      end
-
-      success = 0
-      skipped = 0
-      errors = 0
-
-      ext_records = self.all
+    def self.import_record(ext_record)
+      int_record = int_class.new
       
-      puts "  Importing #{ext_records.count} records..."
-      for ext_record in self.all
-        begin
-          int_record = int_class.new
-          
-          int_record.ean_party = ext_record.field('EAN')
-          int_record.vcard = Vcards::Vcard.new(
-                  :full_name => ext_record.field('DESCR1'),
-                  :street_address => [ext_record.field('ADDR/STREET'), ext_record.field('ADDR/STRNO')].compact.join(" "),
-                  :extended_address => ext_record.field('ADDR/POBOX'),
-                  :postal_code => ext_record.field('ADDR/ZIP'),
-                  :locality => ext_record.field('ADDR/CITY')
-          )
-
-          puts "    " + int_record.to_s
-          
-          int_record.save!
-          success += 1
-        rescue
-          errors += 1
-        end
-      end
+      int_record.ean_party = ext_record.field('EAN')
+      int_record.vcard = Vcards::Vcard.new(
+              :full_name => ext_record.field('DESCR1'),
+              :street_address => [ext_record.field('ADDR/STREET'), ext_record.field('ADDR/STRNO')].compact.join(" "),
+              :extended_address => ext_record.field('ADDR/POBOX'),
+              :postal_code => ext_record.field('ADDR/ZIP'),
+              :locality => ext_record.field('ADDR/CITY')
+      )
       
-      puts
-      puts "  Success: #{success}; skipped: #{skipped}; errors: #{errors}"
-      puts "Import done."
+      return int_record
     end
   end
 end
