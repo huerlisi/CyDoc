ebebbb59128e99368e1a91d1acda6e29236c5672
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-14 22:47:32 +0200

    New import script for Forum Datenaustausch.

---
+++ data/forum_datenaustausch_import.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#  Copyright 2009 Simon Hürlimann <simon.huerlimann@cyt.ch>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+URL='http://www.forum-datenaustausch.ch/'
+
+# Download MS Excel
+function get() {
+local url="${1:-$URL}"
+local output="${2:-$NAME.xls}"
+	wget --no-verbose "$url" --output-document "$output"
+}
+
+function convert() {
+local input="${1:-$NAME.xls}"
+local output="${2:-$NAME.csv}"
+
+	xls2csv "$input" >"$output"
+}
+
+function import() {
+local input="${2:-$NAME.csv}"
+
+	# Import
+	echo "Tarifcodes::Base.import_all(true)" | ../script/console
+}
+
+function cleanup() {
+	rm "$NAME.xls"
+	rm "$NAME.csv"
+}
+
+function list() {
+	echo "tarif_code" "20090402"
+	echo "response_code" "20081027"
+	echo "status_and_validity" "20060901"
+}
+
+function get_all() {
+	list | while read ident version ; do
+		NAME="${ident}_${version}"
+		get "${URL}${NAME}.xls"
+		convert
+		import
+		cleanup
+	done
+}
+
+# Main
+# ====
+function main() {
+	get
+	convert
+	import
+	cleanup
+}
+
+$@
