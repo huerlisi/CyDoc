f024ab6d2692b9fc4126593b14eb77f16edc1e31
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-17 14:33:54 +0200

    Implement VAT model.

--- app/models/vat_class.rb
+++ app/models/vat_class.rb
@@ -1,8 +1,12 @@
-class DrugPrice < ActiveRecord::Base
-  belongs_to :drug_article
-
+class VatClass < ActiveRecord::Base
   named_scope :valid, :conditions => ['valid_from <= ?', Date.today]
   named_scope :current, :conditions => ['valid_from <= ?', Date.today], :order => 'valid_from DESC', :limit => 1
 
-  named_scope :public, :conditions => ['price_type = ?', 'PPUB']
+  def code_name
+    case code
+      when 'reduced': "Reduziert"
+      when 'full': "Normal"
+      when 'excluded': "Ausgenommen"
+    end
+  end
 end
---
+++ db/migrate/20090417112106_create_vat_classes.rb
@@ -0,0 +1,15 @@
+class CreateVatClasses < ActiveRecord::Migration
+  def self.up
+    create_table :vat_classes do |t|
+      t.date :valid_from
+      t.float :rate
+      t.string :code
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :vat_classes
+  end
+end
