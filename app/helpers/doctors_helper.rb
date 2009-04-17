e39129f443176a405317ddb12042c1960ed313be
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-17 09:29:56 +0200

    Use same tabbing for doctors as for patients. (Closes #379)

--- app/helpers/doctors_helper.rb
+++ app/helpers/doctors_helper.rb
@@ -1,6 +1,6 @@
 module DoctorsHelper
   def doctor_tabs(doctor)
-    tabs = [{:name => 'overview', :partial => 'doctors/show', :label => doctor.to_s},
+    tabs = [{:name => 'personal', :partial => 'doctors/show', :label => 'Personalien'},
             {:name => 'accounting', :partial => 'doctors/accounting_tab', :label => 'Konten'},
             {:name => 'printing', :partial => 'doctors/printing_tab', :label => 'Drucker Einstellungen'},
             ]
