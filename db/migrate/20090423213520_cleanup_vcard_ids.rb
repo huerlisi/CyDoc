9c1b4c7091ca4301563b3e8267b172f91c583a4a
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-23 23:41:33 +0200

    Cleanup vcard id schema.

---
+++ db/migrate/20090423213520_cleanup_vcard_ids.rb
@@ -0,0 +1,14 @@
+class CleanupVcardIds < ActiveRecord::Migration
+  def self.up
+    drop_table :patient_vcards
+    
+    remove_column :patients, :billing_vcard_id
+
+    remove_column :vcards, :address
+    remove_column :vcards, :address_id
+    remove_column :vcards, :billing_address_id
+  end
+
+  def self.down
+  end
+end
