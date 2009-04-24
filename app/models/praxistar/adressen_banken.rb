778a96863d6e930d787c461744b33b43f55f285f
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-24 12:55:36 +0200

    Port Praxistar Bank importer to new framework.

--- app/models/praxistar/adressen_banken.rb
+++ app/models/praxistar/adressen_banken.rb
@@ -7,30 +7,18 @@ module Praxistar
       Accounting::Bank
     end
 
-    def self.import_attributes(a)
-      {
-        :vcard => Vcards::Vcard.new(
-  #        :phone_number => a.tx_Telefon,
-          :locality => a.tx_Ort,
-  #        :fax_number => a.tx_FAX,
-          :extended_address => a.tx_ZuHanden,
-          :postal_code => a.tx_PLZ,
-          :street_address => a.tx_Strasse,
-          :full_name => a.tx_Bank
-        )
-     }
-    end
+    def self.import_record(a)
+      int_record = int_class.new(
+        :phone_number => a.tx_Telefon,
+        :locality => a.tx_Ort,
+        :fax_number => a.tx_Fax,
+        :extended_address => a.tx_ZuHanden,
+        :postal_code => a.tx_PLZ,
+        :street_address => a.tx_Strasse,
+        :full_name => a.tx_Bank
+      )
 
-    def self.export_attributes(hozr_record, new_record)
-      {
-        :tx_Telefon => hozr_record.phone_number,
-        :tx_Ort => hozr_record.locality,
-        :tx_FAX => hozr_record.fax_number,
-        :tx_ZuHanden => hozr_record.extended_address,
-        :tx_PLZ => hozr_record.postal_code,
-        :tx_Strasse => hozr_record.street_address,
-        :tx_Bank => hozr_record.name
-      }
+      return int_record
     end
   end
 end
