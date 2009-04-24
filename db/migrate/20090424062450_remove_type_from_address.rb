06e7610b0f2b797bbaa2f545940e26163d525f30
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-24 08:42:50 +0200

    Cleanup addresses table.

---
+++ db/migrate/20090424062450_remove_type_from_address.rb
@@ -0,0 +1,9 @@
+class RemoveTypeFromAddress < ActiveRecord::Migration
+  def self.up
+    remove_column :addresses, :type
+  end
+
+  def self.down
+    add_column :addresses, :type, :string
+  end
+end
