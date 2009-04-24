9f201e25bc1814f00af0191deb2c46069a49a2c6
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-24 09:38:06 +0200

    Nicer code to import doctors from Praxistar.

--- app/models/praxistar/adressen_aerzte.rb
+++ app/models/praxistar/adressen_aerzte.rb
@@ -16,25 +16,25 @@ module Praxistar
         :ean_party => a.tx_EANNr
       )
       
-      praxis_vcard = int_record.vcards.build(
+      int_record.vcards.build(
         :locality => a.tx_Prax_Ort,
         :postal_code => a.tx_Prax_PLZ,
         :street_address => a.tx_Prax_Strasse,
         :family_name => a.tx_Name,
-        :given_name => a.tx_Vorname
+        :given_name => a.tx_Vorname,
+        :phone_number => a.tx_Prax_Telefon1,
+        :fax_number => a.tx_Prax_Fax
       )
-      praxis_vcard.fax_numbers.build(:number => a.tx_Prax_Fax)
-      praxis_vcard.phone_numbers.build(:number => a.tx_Prax_Telefon1)
       
-      private_vcard = int_record.vcards.build(
+      int_record.vcards.build(
         :locality => a.tx_Priv_Ort,
         :postal_code => a.tx_Priv_PLZ,
         :street_address => a.tx_Priv_Strasse,
         :family_name => a.tx_Name,
-        :given_name => a.tx_Vorname
+        :given_name => a.tx_Vorname,
+        :phone_number => a.tx_Priv_Telefon1,
+        :fax_number => a.tx_Priv_Fax
       )
-      private_vcard.fax_numbers.build(:number => a.tx_Priv_Fax)
-      private_vcard.phone_numbers.build(:number => a.tx_Priv_Telefon1)
       
       return int_record
     end
