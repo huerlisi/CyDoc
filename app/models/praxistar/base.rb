f64d04fddc2e5bc4c24b635231237f1babc179f0
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-24 13:10:42 +0200

    Drop legacy importer class for Praxistar.

--- app/models/praxistar/base.rb
+++ app/models/praxistar/base.rb
@@ -6,9 +6,13 @@ module Praxistar
     use_db :prefix => "praxis_"
 
     def self.import_all(do_clean, options = {})
+      # Adresses
       AdressenVersicherungen.import(do_clean, options)
-      PatientenPersonalien.import(do_clean, options)
+      AdressenBanken.import(do_clean, options)
       AdressenPersonal.import(do_clean, options)
+      
+      # Patients
+      PatientenPersonalien.import(do_clean, options)
     end
   end
 end
