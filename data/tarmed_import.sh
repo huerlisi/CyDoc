75701c2a57b793feae6cb2a47f37bb90f38bea4b
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-27 14:33:58 +0200

    Reworked tarmed_import script.

--- data/tarmed_import.sh
+++ data/tarmed_import.sh
@@ -12,20 +12,75 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 
+URL='http://www.tarmedsuisse.ch/fileadmin/media/Dateien/Browser_V_1.06/TARMED_Database_V01.06.zip'
+
+NAME="tarmed"
+
+function check_cmd() {
+local cmd=$1
+
+	if which $cmd >/dev/null ; then
+		echo "Found '$cmd'"
+	else
+		echo "[ERROR] $cmd not found"
+		exit 1
+	fi
+}
+
+function check() {
+	for cmd in wget mdb-schema mdb-tables mdb-export sqlite3 ; do
+		check_cmd $cmd
+	done
+}
+
 # Download MS Access DB
-wget http://www.tarmedsuisse.ch/fileadmin/media/Dateien/Browser_V_1.06/TARMED_Database_V01.06.zip
-unzip TARMED_Database_V01.06.zip
+function get() {
+local url="${1:-$URL}"
+local output="${2:-$NAME.zip}"
+	wget --no-verbose "$url" --output-document "$output"
+	unzip "$output"
+	mv TARMED_Database_V01.06.mdb $NAME.mdb
+}
 
 # Create sqlite3 db
-./tarmed_convert.sh --sqlite3 TARMED_Database_V01.06.mdb | sqlite3 ../db/tarmed.sqlite3
+function convert() {
+local input="${1:-$NAME.mdb}"
+local output="${2:-$NAME.sqlite3}"
 
-# Create links
-ln -s tarmed.sqlite3 ../db/tarmed_development.sqlite3
-ln -s tarmed.sqlite3 ../db/tarmed_production.sqlite3
-ln -s tarmed.sqlite3 ../db/tarmed_test.sqlite3
+	./tarmed_convert.sh --sqlite3 "$input" | sqlite3 "$output"
+
+	# Create links
+	ln -fs "../data/$output" ../db/tarmed_development.sqlite3
+	ln -fs "../data/$output" ../db/tarmed_production.sqlite3
+	ln -fs "../data/$output" ../db/tarmed_test.sqlite3
+}
+
+function import() {
+local input="${2:-$NAME.csv}"
+
+	# Import as TariffItems
+	echo "Tarmed::Base.import_all(true)" | ../script/console
+}
 
 # Cleanup
-rm TARMED_Database_V01.06.zip TARMED_Database_V01.06.mdb
+function cleanup() {
+	rm "$NAME.zip"
+	rm "$NAME.mdb"
+}
 
-# Import as TariffItems
-echo "Tarmed::Base.import" | ../script/console
+# Main
+# ====
+function main() {
+	check
+	get
+	convert
+	import
+	cleanup
+}
+
+if [ $# == 0 ] ; then
+	main
+else
+	$1
+fi
+
