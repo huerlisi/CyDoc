a868d14659680726e507d25bdb5b794ecaed29a3
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-04-28 23:29:39 +0200

    Allow specifying path option for analyseliste import.

--- app/models/analyseliste/base.rb
+++ app/models/analyseliste/base.rb
@@ -4,7 +4,9 @@ module Analyseliste
   class Base
     include Importer
 
-    def self.path
+    def self.path(options = {})
+      return options[:input] if options[:input]
+
       case ENV['RAILS_ENV']
         when 'production': File.join(RAILS_ROOT, 'data', 'analyseliste.csv')
         when 'development', 'test': File.join(RAILS_ROOT, 'test', 'fixtures', 'analyseliste', 'analyseliste.csv')
@@ -12,10 +14,11 @@ module Analyseliste
     end
 
     def self.find(selector, options = {})
-      FasterCSV.read(path, :headers => true)
+      @@data
     end
 
     def self.import_all(do_clean = false, options = {})
+      @@data = FasterCSV.read(path(options), :headers => true)
       LabTariffItem.import(do_clean)
     end
   end
