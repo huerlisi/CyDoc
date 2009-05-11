6e69ab6d35dbcea643ef21c65520949f3e3a9229
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-05-11 22:34:36 +0200

    More Cyto cleanups.

---
+++ db/migrate/20090511184208_remove_account_id_from_doctor.rb
@@ -0,0 +1,9 @@
+class RemoveAccountIdFromDoctor < ActiveRecord::Migration
+  def self.up
+    remove_column :doctors, :account_id
+  end
+
+  def self.down
+    add_column :doctors, :account_id, :integer
+  end
+end
---
+++ db/migrate/20090511184820_cleanup_more_cyto_tables.rb
@@ -0,0 +1,12 @@
+class CleanupMoreCytoTables < ActiveRecord::Migration
+  def self.up
+    drop_table :finding_groups
+    drop_table :mailings
+    drop_table :order_forms
+   
+    remove_column :medical_cases, :zytolabor_case_id
+  end
+
+  def self.down
+  end
+end
