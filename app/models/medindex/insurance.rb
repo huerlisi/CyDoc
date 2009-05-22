825741fe7e6cfa38d8a5fa1f0090e700ce9aa89a
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-05-22 23:57:17 +0200

    Fix multiple vcards created during insurance report from Medindex.

--- app/models/medindex/insurance.rb
+++ app/models/medindex/insurance.rb
@@ -11,9 +11,13 @@ module Medindex
     def tag_start(name, attrs)
       super
       
-      case name
-        when 'ADDR': @vcard = @int_record.vcards.build
-      end
+#      case name
+#        when record_name: @vcard = @int_record.vcards.build
+
+#      TODO: only create vcard for additional addresses
+#      case name
+#        when 'ADDR': @vcard = @int_record.vcards.build
+#      end
     end
     
     def tag_end(name)
@@ -22,13 +26,15 @@ module Medindex
           @int_record.save!
           puts @int_record
 
-        when 'EAN': @int_record.ean_party = @text
+        when 'EAN':       @int_record.ean_party       = @text
         when 'GROUP_EAN': @int_record.group_ean_party = @text
-        when 'ROLE': @int_record.role = @text
-        when 'DESCR1': @int_record.full_name = @text
-        when 'ZIP': @int_record.postal_code = @text
-        when 'CITY': @int_record.locality = @text
-        when 'POBOX': @int_record.extended_address = @text
+        when 'ROLE':      @int_record.role            = @text
+      # Vcard
+#        when 'ADDR' :  @vcard.save!
+        when 'DESCR1': @int_record.full_name        = @text
+        when 'ZIP':    @int_record.postal_code      = @text
+        when 'CITY':   @int_record.locality         = @text
+        when 'POBOX':  @int_record.extended_address = @text
       end
     end
   end
