8364c8b9a688b42adb6f45329d6271fe99e8427a
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-06-02 15:15:30 +0200

    Port Praxistar importer for Patients to new vcards framework.

--- app/models/praxistar/patienten_personalien.rb
+++ app/models/praxistar/patienten_personalien.rb
@@ -8,39 +8,41 @@ module Praxistar
     end
     
     def self.import_record(a, options)
-      int_record = int_class.new({
-        :vcard => Vcards::Vcard.new(
-          :locality => a.tx_Ort,
-          :postal_code => a.tx_PLZ,
-          :street_address => a.tx_Strasse,
-          :family_name => a.tx_Name,
-          :given_name => a.tx_Vorname,
-          :honorific_prefix => [a.tx_Anrede, a.tx_Titel].select{|f| !f.blank?}.join(' ')
-        ),
-        :billing_vcard => Vcards::Vcard.new(
-          :locality => a.tx_fakt_Ort,
-          :postal_code => a.tx_fakt_PLZ,
-          :street_address => a.tx_fakt_Strasse,
-          :family_name => a.tx_fakt_Name,
-          :given_name => a.tx_fakt_Vorname,
-          :extended_address => a.tx_fakt_ZuHanden,
-          :honorific_prefix => [a.tx_fakt_Anrede, a.tx_fakt_Titel].select{|f| !f.blank?}.join('')
-        ),
-#        :insurance_id => a.KK_Garant_ID,
+      int_record = int_class.new(
 
 	:doctor_patient_nr => a.ln_Patienten_Nr,
 
+#        :insurance_id => a.KK_Garant_ID,
         :insurance_nr => a.tx_KK_MitgliedNr,
         :doctor_id => a.ZuwArzt_ID,
+
         :birth_date => a.tx_Geburtsdatum,
         :created_at => a.tx_Aufnahmedatum,
         :remarks => a.mo_Bemerkung,
         :sex => a.Geschlecht_ID,
         :dunning_stop => a.tf_Mahnen,
         :use_billing_address => a.tf_fakt_Aktiv,
-        :deceased => a.tf_Exitus,
-      })
+        :deceased => a.tf_Exitus
+      )
       
+      int_record.vcards.build(
+        :locality => a.tx_Ort,
+        :postal_code => a.tx_PLZ,
+        :street_address => a.tx_Strasse,
+        :family_name => a.tx_Name,
+        :given_name => a.tx_Vorname,
+        :honorific_prefix => [a.tx_Anrede, a.tx_Titel].select{|f| !f.blank?}.join(' ')
+      )
+      int_record.vcards.build(
+        :locality => a.tx_fakt_Ort,
+        :postal_code => a.tx_fakt_PLZ,
+        :street_address => a.tx_fakt_Strasse,
+        :family_name => a.tx_fakt_Name,
+        :given_name => a.tx_fakt_Vorname,
+        :extended_address => a.tx_fakt_ZuHanden,
+        :honorific_prefix => [a.tx_fakt_Anrede, a.tx_fakt_Titel].select{|f| !f.blank?}.join('')
+      )
+
       return int_record
     end
   end
