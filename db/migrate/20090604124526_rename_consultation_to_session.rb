90ad410225d13c041b302dd3a4a1dcd374b6e78f
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-06-04 15:00:21 +0200

    Start implementing session support.

---
+++ db/migrate/20090604124526_rename_consultation_to_session.rb
@@ -0,0 +1,11 @@
+class RenameConsultationToSession < ActiveRecord::Migration
+  def self.up
+    rename_table :consultations, :sessions
+    
+    remove_column :sessions, :doctor_id
+    add_column :sessions, :state, :string, :default => 'open'
+  end
+
+  def self.down
+  end
+end
---
+++ test/unit/session_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class SessionTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
