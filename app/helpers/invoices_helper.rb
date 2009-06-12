173a43a6483e3a126c7358e8fc0aaac3c6d83f73
simon.huerlimann@cyt.ch <simon.huerlimann@cyt.ch>
2009-06-12 15:56:42 +0200

    Some invoice overview works.

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,8 +1,10 @@
 module InvoicesHelper
   def invoice_tabs
-    tabs = [{:name => 'insurance_recipe', :partial => 'invoices/insurance_recipe', :label => 'Rückforderungsbeleg'},
-            {:name => 'patient_letter', :partial => 'invoices/patient_letter', :label => 'Rechnung'}
-            ]
+    tabs = [
+      {:name => 'overview', :partial => 'invoices/show', :label => 'Übersicht'},
+      {:name => 'insurance_recipe', :partial => 'invoices/insurance_recipe', :label => 'Rückforderungsbeleg'},
+      {:name => 'patient_letter', :partial => 'invoices/patient_letter', :label => 'Rechnung'}
+    ]
     # TODO check authorization, have a look at redmine
   end
 end
