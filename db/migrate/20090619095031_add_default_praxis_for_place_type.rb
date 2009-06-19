81d3bbf74a458cef4cea80e274ef30e84d401492
Simon Hürlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-06-19 11:59:57 +0200

    Add default for place type.

---
+++ db/migrate/20090619095031_add_default_praxis_for_place_type.rb
@@ -0,0 +1,12 @@
+class AddDefaultPraxisForPlaceType < ActiveRecord::Migration
+  def self.up
+    change_column :invoices, :place_type, :string, :default => 'Praxis'
+    Invoice.update_all "place_type = 'Praxis'", "place_type IS NULL"
+    
+    change_column :treatments, :place_type, :string, :default => 'Praxis'
+    Treatment.update_all "place_type = 'Praxis'", "place_type IS NULL"
+  end
+
+  def self.down
+  end
+end
