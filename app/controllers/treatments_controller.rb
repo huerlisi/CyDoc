f80b364e2d1ab00ad369aca28142ff040741809a
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-07-29 00:01:19 +0200

    Show session date in title (Closes #509)

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -52,7 +52,7 @@ class TreatmentsController < ApplicationController
 #    @treatment.canton ||= @tiers.provider.vcard.address.region
 
     # Services
-    @treatment.sessions.build
+    @treatment.sessions.build(:duration_from => DateTime.now)
 #    @treatment.diagnoses = medical_cases.map{|medical_case| medical_case.diagnosis}
 
     # Saving
