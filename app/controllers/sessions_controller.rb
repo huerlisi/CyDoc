165cdd14ee86a046371ebfeb1ca686d16fbec7e4
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-07-29 09:49:32 +0200

    Implement new session creation for treatment. (Closes #484, #479)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  # GET /sessions/new
+  def new
+    @patient = Patient.find(params[:patient_id])
+    @treatment = Treatment.find(params[:treatment_id])
+    
+    @session = @treatment.sessions.create(:duration_from => DateTime.now)
+
+    respond_to do |format|
+      format.html { }
+      format.js {
+        render :update do |page|
+          page.replace_html 'session_list', :partial => 'list', :object => @treatment.sessions
+        end
+      }
+    end
+  end
 end
