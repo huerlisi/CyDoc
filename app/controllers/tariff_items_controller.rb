2f468268989d1b57243f2091cffd13d3292dce9b
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-07-31 10:25:50 +0200

    Use tab for tariff catalog. (Closes #313)

--- app/controllers/tariff_items_controller.rb
+++ app/controllers/tariff_items_controller.rb
@@ -1,10 +1,10 @@
 class TariffItemsController < ApplicationController
   # GET /tariff_items
-  def index
+  def search
     query = params[:query]
     query ||= params[:search][:query] if params[:search]
 
-    @tariff_items = TariffItem.clever_find(query).paginate(:page => params['page'])
+    @tariff_items = TariffItem.clever_find(query).paginate(:page => params['page'], :per_page => 25)
     respond_to do |format|
       format.html {
         render :action => 'list'
