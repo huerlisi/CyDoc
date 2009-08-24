b994ba35b593751d06eab62ef7cda0d26992f53f
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-08-24 23:06:26 +0200

    Update patients table to use empty string as default for remarks.

---
+++ db/migrate/20090824205834_no_empty_patient_remarks.rb
@@ -0,0 +1,8 @@
+class NoEmptyPatientRemarks < ActiveRecord::Migration
+  def self.up
+    change_column :patients, :remarks, :text, :default => '', :null => false
+  end
+
+  def self.down
+  end
+end
