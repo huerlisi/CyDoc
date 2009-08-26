eb6f158c40c277e09061beb2840d02ff77552dcb
simon.huerlimann@cyt.ch <simon.huerlimann@cyt.ch>
2009-08-26 15:21:33 +0200

    Implement in_place edit for bookings on account view.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,11 @@
+include Accounting
+
 class AccountsController < ApplicationController
+  in_place_edit_for :booking, :amount
+  in_place_edit_for :booking, :value_date
+  in_place_edit_for :booking, :title
+  in_place_edit_for :booking, :comments
+
   # GET /accounts
   def index
     @accounts = Accounting::Account.paginate(:page => params['page'], :per_page => 20, :order => 'code')
