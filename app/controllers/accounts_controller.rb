ea9ef78d7f5f72063b5fa31584e1809718fdf11e
simon.huerlimann@cyt.ch <simon.huerlimann@cyt.ch>
2009-08-26 13:53:49 +0200

    Implement initial version of account view.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,24 @@
 class AccountsController < ApplicationController
+  # GET /accounts
   def index
-    @accounts = Accounting::Account.paginate(:page => params['page'], :per_page => 20, :order => 'updated_at DESC')
+    @accounts = Accounting::Account.paginate(:page => params['page'], :per_page => 20, :order => 'code')
     
     respond_to do |format|
       format.html {
         render :action => 'list'
       }
+    end
+  end
+
+  # GET /accounts/1
+  def show
+    @account = Accounting::Account.find(params[:id])
+
+    respond_to do |format|
+      format.html {
+        render :action => 'show'
+      }
+      format.js { }
     end
   end
 end
--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,5 @@
 module AccountsHelper
+  def currency_fmt(value)
+    sprintf("%.2f", value)
+  end
 end
