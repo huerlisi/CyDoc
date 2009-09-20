7035a02d6bd210673e29004585aceef7ad1335c8
simon.huerlimann@cyt.ch <simon.huerlimann@cyt.ch>
2009-09-20 22:08:38 +0200

    New migration add_imported_ids.

---
+++ db/migrate/20090910125218_add_imported_ids.rb
@@ -0,0 +1,17 @@
+class AddImportedIds < ActiveRecord::Migration
+  def self.up
+    add_column :patients, :imported_id, :integer
+    add_column :treatments, :imported_id, :integer
+    add_column :sessions, :imported_id, :integer
+    add_column :service_records, :imported_id, :integer
+    add_column :medical_cases, :imported_id, :integer
+  end
+
+  def self.down
+    remove_column :patients, :imported_id
+    remove_column :treatments, :imported_id
+    remove_column :sessions, :imported_id
+    remove_column :service_records, :imported_id
+    remove_column :medical_cases, :imported_id
+  end
+end
