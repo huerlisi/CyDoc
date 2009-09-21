34e6296f770a42819a10e95156a43fb473c41850
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-09-21 22:56:00 +0200

    Explicit validation of treatment and patient presence in Invoice.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -39,11 +39,15 @@ class Invoice < ActiveRecord::Base
   has_and_belongs_to_many :service_records, :order => 'tariff_type, date DESC, if(ref_code IS NULL, code, ref_code), concat(code,ref_code)'
 
   # Validation
-  validates_presence_of :service_records, :message => 'Keine Leistung eingegeben.'
   validates_format_of :value_date, :with => /[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{2,4}/, :message => 'braucht Format dd.mm.yy (z.B. 3.12.1980)'
 
+  validates_presence_of :service_records, :message => 'Keine Leistung eingegeben.'
   validate :valid_service_records?
+  
+  validates_presence_of :treatment
   validate :valid_treatment?
+
+  validates_presence_of :patient
   validate :valid_patient?
   
   def valid_service_records?
