bb0c8c7cf374e2b5b884d01af7c40808246221bb
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-09-23 16:04:04 +0200

    Port Patient importer to new framework.

--- app/models/praxidata/doctor.rb
+++ app/models/praxidata/doctor.rb
@@ -3,12 +3,14 @@ module Praxidata
     def import(import_record)
       self.attributes = {
         :remarks    => import_record.moBemerkungen,
+        :active     => !(import_record.tfInaktiv?),
         :speciality => import_record.arzt.txSpezialgebiet,
         :ean_party  => import_record.txEANNummer,
         :vcard      => Vcards::Vcard.new(
-          :honorific_prefix => import_record.txAnrede,
+          :honorific_prefix => [import_record.txAnrede, import_record.person.txTitel].compact.join(' '),
           :family_name      => import_record.txName1,
           :given_name       => import_record.txName2,
+          :nickname         => import_record.person.txNickName,
           :street_address   => import_record.adresse.txAdresse1,
           :extended_address => import_record.adresse.txAdresse2,
           #:extended_address => import_record.adresse.txAdresse3,
