42e128040dd272b8932a9d572e03de9baa5fc433
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-10-29 10:52:35 +0100

    Add some index to vcards

---
+++ db/migrate/20091029094646_add_index_to_vcards.rb
@@ -0,0 +1,9 @@
+class AddIndexToVcards < ActiveRecord::Migration
+  def self.up
+    add_index :vcards, [:object_id, :object_type]
+  end
+
+  def self.down
+    remove_index :vcards, :column => [:object_id, :object_type]
+  end
+end
