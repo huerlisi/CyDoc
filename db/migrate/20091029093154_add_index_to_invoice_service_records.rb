30894bd56318051d23ae0beffb6c1ccffaede29e
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-10-29 10:34:52 +0100

    Add index to invoices_service_records.

---
+++ db/migrate/20091029093154_add_index_to_invoice_service_records.rb
@@ -0,0 +1,11 @@
+class AddIndexToInvoiceServiceRecords < ActiveRecord::Migration
+  def self.up
+    add_index :invoices_service_records, :invoice_id
+    add_index :invoices_service_records, :service_record_id
+  end
+
+  def self.down
+    remove_index :invoices_service_records, :invoice_id
+    remove_index :invoices_service_records, :service_record_id
+  end
+end
