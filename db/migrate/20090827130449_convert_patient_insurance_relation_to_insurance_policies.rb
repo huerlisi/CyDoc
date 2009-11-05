69f98c46169f40cbc912db0f7d3c8a7b7d793e62
Simon Huerlimann <Simon Huerlimann simon.huerlimann@cyt.ch>
2009-11-05 11:12:32 +0100

    Only create insurance_policy during migration if there has been an insurance before.

--- db/migrate/20090827130449_convert_patient_insurance_relation_to_insurance_policies.rb
+++ db/migrate/20090827130449_convert_patient_insurance_relation_to_insurance_policies.rb
@@ -7,11 +7,13 @@ class ConvertPatientInsuranceRelationToInsurancePolicies < ActiveRecord::Migrati
 
   def self.up
     for patient in Patient.all
-      patient.insurance_policies.create(
-        :insurance   => patient.insurance,
-        :number      => patient.insurance_nr,
-        :policy_type => 'KVG'
-      )
+      if patient.insurance
+        patient.insurance_policies.create(
+          :insurance   => patient.insurance,
+          :number      => patient.insurance_nr,
+          :policy_type => 'KVG'
+        )
+      end
     end
     
     remove_columns :patients, :insurance_id, :insurance_nr
