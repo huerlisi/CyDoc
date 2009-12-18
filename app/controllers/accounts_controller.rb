53de7b4109d001cfcbffe5d1809691b206d01e3f
Simon Hürlimann <simon.huerlimann@cyt.ch>
2009-12-18 15:17:57 +0100

    Add simple pagination to account views. Unfinished, yet.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -20,7 +20,11 @@ class AccountsController < ApplicationController
   # GET /accounts/1
   def show
     @account = Accounting::Account.find(params[:id])
-
+    
+    # We're getting hit by will_paginate bug #120 (http://sod.lighthouseapp.com/projects/17958/tickets/120-paginate-association-with-finder_sql-raises-typeerror)
+    # This needs the will_paginate version from http://github.com/jwood/will_paginate/tree/master to work.
+    @bookings = @account.bookings.paginate(:page => params['page'], :per_page => 20, :order => 'value_date')
+    
     respond_to do |format|
       format.html {
         render :action => 'show'
