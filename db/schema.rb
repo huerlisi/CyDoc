3237e0026ffcdaf5b1db53309255732ac5328b39
Simon Hürlimann <simon.huerlimann@cyt.ch>
2010-01-11 07:48:28 +0100

    New schema.rb version.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091230230943) do
+ActiveRecord::Schema.define(:version => 20100108124944) do
 
   create_table "accounts", :force => true do |t|
     t.string   "number"
