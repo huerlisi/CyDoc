1727252bdfea3f1b5dfb7260787436abe240f645
Simon Hürlimann <simon.huerlimann@cyt.ch>
2010-01-17 22:41:47 +0100

    Port accounts view to has_scope.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,24 +4,11 @@ class AccountsController < ApplicationController
   in_place_edit_for :booking, :title
   in_place_edit_for :booking, :comments
 
-  # Filters
-  around_filter Accounting::Booking
-  
-  def value_date_scope
-    session[:value_date_scope]
-  end
-
-  def value_date_scope=(value)
-    if value.nil?
-      session[:value_date_scope] = nil
-    else
-      year = value.to_i
-      session[:value_date_scope] = Date.new(year, 1, 1)..Date.new(year, 12, 31)
-    end
-  end
+  # Scopes
+  has_scope :by_value_date, :type => :range, :session => 'has_scope'
   
   def set_value_date_filter
-    self.value_date_scope = params[:year]
+    apply_scopes(Accounting::Booking)
     
     redirect_to params[:uri]
   end
@@ -43,7 +30,7 @@ class AccountsController < ApplicationController
     
     # We're getting hit by will_paginate bug #120 (http://sod.lighthouseapp.com/projects/17958/tickets/120-paginate-association-with-finder_sql-raises-typeerror)
     # This needs the will_paginate version from http://github.com/jwood/will_paginate/tree/master to work.
-    @bookings = @account.bookings.paginate(:page => params['page'], :per_page => 20, :order => 'value_date, id')
+    @bookings = apply_scopes(Accounting::Booking).by_account(@account).paginate(:page => params['page'], :per_page => 20, :order => 'value_date, id')
     respond_to do |format|
       format.html {
         render :action => 'show'
