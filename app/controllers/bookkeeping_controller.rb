9f0ba3ed89a25072766253c00b48ff3297947685
Simon Hürlimann <simon.huerlimann@cyt.ch>
2010-02-02 00:14:12 +0100

    Move bookkeeping report logic from view to controller.

--- app/controllers/bookkeeping_controller.rb
+++ app/controllers/bookkeeping_controller.rb
@@ -1,2 +1,10 @@
 class BookkeepingController < ApplicationController
+  def report
+    @total_invoiced    = -Invoice::EARNINGS_ACCOUNT.saldo('2009-01-01'..'2009-12-31')
+    @total_paid        = Accounting::Account.find_by_code('1000').saldo('2009-01-01'..'2009-12-31') + Accounting::Account.find_by_code('1020').saldo('2009-01-01'..'2009-12-31')
+    @open_items        = Invoice::DEBIT_ACCOUNT.saldo('2009-12-31')
+    @debtors_write_off = Accounting::Account.find_by_code('3900').saldo('2009-01-01'..'2009-12-31')
+    @started_work      = Session.find(:all, :conditions => "duration_from < '2009-12-31'").select{|s| s.treatment.invoices.empty?}.to_a.sum(&:amount)
+    @drugs_stock       = Accounting::Account.find_by_code('1210').saldo('2009-12-31')
+  end
 end
