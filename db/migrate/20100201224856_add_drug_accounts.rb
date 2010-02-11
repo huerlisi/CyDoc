6998764b3a068baca41178402dd9a69ded82d52d
Simon Hürlimann <simon.huerlimann@cyt.ch>
2010-02-11 10:45:33 +0100

    Add migration to create drug account if it not exists, yet.

---
+++ db/migrate/20100201224856_add_drug_accounts.rb
@@ -0,0 +1,9 @@
+class AddDrugAccounts < ActiveRecord::Migration
+  def self.up
+    Accounting::Account.new(:code => '1210', :title => 'Lager Medikamente').save unless Accounting::Account.find_by_code('1210')
+    Accounting::Account.new(:code => '4000', :title => 'Aufwand Medikamente').save unless Accounting::Account.find_by_code('4000')
+  end
+
+  def self.down
+  end
+end
