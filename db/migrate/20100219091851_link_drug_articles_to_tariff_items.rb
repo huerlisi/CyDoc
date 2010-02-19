7054b9b30106c9bc3cfdd59abbdf373067d3724f
Simon Hürlimann <simon.huerlimann@cyt.ch>
2010-02-19 10:21:42 +0100

    Add migration to link drug tariff_items to drug articles. (Closes #1015)

---
+++ db/migrate/20100219091851_link_drug_articles_to_tariff_items.rb
@@ -0,0 +1,8 @@
+class LinkDrugArticlesToTariffItems < ActiveRecord::Migration
+  def self.up
+    DrugTariffItem.all.map{|t| a = DrugArticle.find_by_code(t.code); t.imported = a; t.save; puts "#{t.to_s} => #{a.to_s}"}
+  end
+
+  def self.down
+  end
+end
