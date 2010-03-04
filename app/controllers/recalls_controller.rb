b432ae64c78267056e880723162b89485b7346ac
Simon Hürlimann <simon.huerlimann@cyt.ch>
2010-03-04 09:56:15 +0100

    Implement recall form for patient.

--- app/controllers/recalls_controller.rb
+++ app/controllers/recalls_controller.rb
@@ -1,2 +1,17 @@
 class RecallsController < ApplicationController
+  # GET /recalls/new
+  def new
+    @patient = Patient.find(params[:patient_id])
+    
+    @recall = @patient.recalls.build
+
+    respond_to do |format|
+      format.html { }
+      format.js {
+        render :update do |page|
+          page.replace_html "new_recall", :partial => 'form'
+        end
+      }
+    end
+  end
 end
