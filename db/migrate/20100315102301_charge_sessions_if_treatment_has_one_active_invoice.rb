5b4c7748cc0a69cfc9c7e93f263f0205ee0dd7fd
Simon Hürlimann <simon.huerlimann@cyt.ch>
2010-03-15 11:24:33 +0100

    Migration to charge sessions if treatment has one active invoice. (Closes #901)

---
+++ db/migrate/20100315102301_charge_sessions_if_treatment_has_one_active_invoice.rb
@@ -0,0 +1,13 @@
+class ChargeSessionsIfTreatmentHasOneActiveInvoice < ActiveRecord::Migration
+  def self.up
+    for t in Treatment.all
+      next if t.invoices.active.count != 1
+      
+      i = t.invoices.active.first
+      t.sessions.map{|s| s.invoice = i; s.state = 'charged'; s.save}
+    end
+  end
+
+  def self.down
+  end
+end
