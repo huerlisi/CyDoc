dacd98c61763effc2ff1674507a29aee23ebbe4f
Simon Hürlimann <simon.huerlimann@cyt.ch>
2010-03-31 11:33:31 +0200

    Add sent_at and proposed appointment to recall list.

---
+++ db/migrate/20100331084713_add_sent_at_to_recalls.rb
@@ -0,0 +1,9 @@
+class AddSentAtToRecalls < ActiveRecord::Migration
+  def self.up
+    add_column :recalls, :sent_at, :datetime
+  end
+
+  def self.down
+    remove_column :recalls, :sent_at
+  end
+end
