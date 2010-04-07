14d34c2737565e7d4e76bf813106167d3c51d69b
Simon Hürlimann (ZytoLabor) <simon.huerlimann@cyt.ch>
2010-04-07 09:55:47 +0200

    Add schema version update.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100331084713) do
+ActiveRecord::Schema.define(:version => 20100406200401) do
 
   create_table "accounts", :force => true do |t|
     t.string   "number"
