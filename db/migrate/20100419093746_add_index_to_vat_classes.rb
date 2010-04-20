ce88929e70707098ca85411a43c141c3f796f501
Simon Hürlimann (ZytoLabor) <simon.huerlimann@cyt.ch>
2010-04-20 08:29:47 +0200

    Add index to Vat classes.

---
+++ db/migrate/20100419093746_add_index_to_vat_classes.rb
@@ -0,0 +1,11 @@
+class AddIndexToVatClasses < ActiveRecord::Migration
+  def self.up
+    add_index :vat_classes, :code
+    add_index :vat_classes, :valid_from
+  end
+
+  def self.down
+    remove_index :vat_classes, :code
+    remove_index :vat_classes, :valid_from
+  end
+end
