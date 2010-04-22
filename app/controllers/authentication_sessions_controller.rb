e7c2d82f29f17473fde2e78d6fb42b5a1afe652c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-04-22 09:44:18 +0200

    Redirect to root controller, not / after authentication.

--- app/controllers/authentication_sessions_controller.rb
+++ app/controllers/authentication_sessions_controller.rb
@@ -16,7 +16,7 @@ class AuthenticationSessionsController < ApplicationController
       self.current_user = user
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
-      redirect_back_or_default('/')
+      redirect_back_or_default(:controller => '/')
       flash[:notice] = "Logged in successfully"
     else
       note_failed_signin
@@ -29,7 +29,7 @@ class AuthenticationSessionsController < ApplicationController
   def destroy
     logout_killing_session!
     flash[:notice] = "You have been logged out."
-    redirect_back_or_default('/')
+    redirect_back_or_default(:controller => '/')
   end
 
 protected
