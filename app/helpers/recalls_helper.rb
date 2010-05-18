f79886a26b6db93df7666b7ba9cd6682d26be6eb
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-05-18 15:44:14 +0200

    Add link to last session in recall list.

--- app/helpers/recalls_helper.rb
+++ app/helpers/recalls_helper.rb
@@ -1,2 +1,8 @@
 module RecallsHelper
+  def link_to_last_session(recall)
+    session = recall.patient.treatments.map{|t| t.sessions}.flatten.first
+    return "Kein Termin" if session.nil?
+    
+    return link_to session.duration_from.try(:to_date), treatment_path(session.treatment)
+  end
 end
