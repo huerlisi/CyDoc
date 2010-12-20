8cb08958d16995da673d1849e5d9854d1eeca781
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-20 15:21:16 +0100

    Allow viewing help when not logged in.

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,5 +1,7 @@
 class HelpController < ApplicationController
-
+  # No authentication
+  skip_before_filter :login_required, :authenticate
+  
   def help
       render 'app/views/index.html'
   end
