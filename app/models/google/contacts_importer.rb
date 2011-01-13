6c3b11610924cbd6d8d911743af191ed2691241d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-01-13 10:34:26 +0100

    Add preliminary importer for Google Contacts CSV exports.

---
+++ app/models/google/contacts_importer.rb
@@ -0,0 +1,43 @@
+require 'fastercsv'
+
+module Google
+  class ContactsImporter
+    def self.import(r)
+      p = Patient.new
+      p.birth_date = Date.today
+      
+      p.given_name = r['First Name']
+      p.family_name = r['Last Name']
+      p.remarks = r['Notes'] || ''
+      
+      p.phone_numbers << PhoneNumber.new(:number => r['Home Phone'], :phone_number_type => 'Festnetz (Privat)') if r['Home Phone'].present?
+      p.phone_numbers << PhoneNumber.new(:number => r['Business Phone'], :phone_number_type => 'Festnetz (gesch.)') if r['Business Phone'].present?
+      p.phone_numbers << PhoneNumber.new(:number => r['Mobile Phone'], :phone_number_type => 'Mobile') if r['Mobile Phone'].present?
+      p.phone_numbers << PhoneNumber.new(:number => r['E-mail Address'], :phone_number_type => 'E-Mail') if r['E-mail Address'].present?
+
+      address = r['Business Address']
+      unless address.blank?
+        a = p.vcard.build_address
+        a.street_address = address.split("\n")[0]
+        a.postal_code = address.split("\n")[1].split(' ')[-1]
+        a.locality = address.split("\n")[1].split(' ')[0..-2]
+      end
+      address = r['Home Address']
+      unless address.blank?
+        a = p.vcard.build_address
+        a.street_address = address.split("\n")[0]
+        a.postal_code = address.split("\n")[1].split(' ')[-1]
+        a.locality = address.split("\n")[1].split(' ')[0..-2]
+      end
+      p.save
+    end
+    
+    def self.import_all
+      records = FasterCSV.parse(File.new(File.join(RAILS_ROOT, 'data', 'google', 'contacts.csv')), {:headers => true})
+      
+      for record in records
+        import(record)
+      end
+    end
+  end
+end
