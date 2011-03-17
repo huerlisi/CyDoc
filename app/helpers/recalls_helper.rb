1cf507efdee450648b1ccf3f4db2daddcda32994
Roman Simecek <roman.simecek@cyt.ch>
2011-03-17 17:22:28 +0100

    Translation added.

--- app/helpers/recalls_helper.rb
+++ app/helpers/recalls_helper.rb
@@ -1,7 +1,7 @@
 module RecallsHelper
   def link_to_last_session(recall)
     session = recall.patient.last_session
-    return "Kein Termin" if session.nil?
+    return t('cydoc.recall.no_appointment') if session.nil?
     
     return link_to session.duration_from.try(:to_date), treatment_path(session.treatment)
   end
