06c4e7bad3e1d15d0689455297ce1825cc17102d
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-03-18 12:14:24 +0100

    Use default locale in routing filter.

--- lib/routing.rb
+++ lib/routing.rb
@@ -2,23 +2,20 @@ require 'routing_filter/filter'
 
 module RoutingFilter
   class Locale < Filter
-
-    LOCALE = 'de-CH'
-
     def around_recognize(path, env, &block)
       locale = nil
       path.sub! %r(^/([a-zA-Z]{2}|[a-zA-Z]{2}-[a-zA-Z]{2})(?=/|$)) do locale = $1; '' end
 
       yield.tap do |params|
-        params[:locale] = locale || LOCALE
+        params[:locale] = locale || I18n.default_locale
       end
     end
     
     def around_generate(*args, &block)
-      locale = args.extract_options!.delete(:locale) || LOCALE
+      locale = args.extract_options!.delete(:locale) || I18n.default_locale
 
       yield.tap do |result|
-        if locale != LOCALE
+        if locale != I18n.default_locale
           result.sub!(%r(^(http.?://[^/]*)?(.*))){ "#{$1}/#{locale}#{$2}" }
         end
       end
