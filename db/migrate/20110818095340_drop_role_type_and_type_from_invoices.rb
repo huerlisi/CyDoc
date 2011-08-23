3776718e767c608ddac01e1deb47c4bfa9999dfe
Simon Hürlimann <simon.huerlimann@cyt.ch>
2011-08-23 09:18:53 +0200

    Drop role_type and type from invoices.

---
+++ db/migrate/20110818095340_drop_role_type_and_type_from_invoices.rb
@@ -0,0 +1,9 @@
+class DropRoleTypeAndTypeFromInvoices < ActiveRecord::Migration
+  def self.up
+    remove_column :invoices, :role_type
+    remove_column :invoices, :role_title
+  end
+
+  def self.down
+  end
+end
