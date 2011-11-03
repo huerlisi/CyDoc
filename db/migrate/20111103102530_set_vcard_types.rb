5bc1ac5aded3a159ba7c77cafad3cc23046400c1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-03 11:30:34 +0100

    Set Vcard type for patients.

---
+++ db/migrate/20111103102530_set_vcard_types.rb
@@ -0,0 +1,8 @@
+class SetVcardTypes < ActiveRecord::Migration
+  def self.up
+    Vcard.update_all "vcard_type = 'private'", "vcard_type IS NULL AND object_type = 'Patient'"
+  end
+
+  def self.down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111026092116) do
+ActiveRecord::Schema.define(:version => 20111103102530) do
 
   create_table "accounts", :force => true do |t|
     t.string   "number"
