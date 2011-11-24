a8d703115a2a06db6ef33dd78bf0affafdee8007
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-24 15:04:36 +0100

    Add insurance importer for swiss official list.

---
+++ app/models/insurances/import.rb
@@ -0,0 +1,38 @@
+require 'fastercsv'
+require 'insurance'
+
+module Insurances
+  module Import
+    def import(import_record)
+      self.attributes = {
+        :role            => 'H',
+        :full_name       => import_record[2],
+        :street_address  => import_record[3],
+        :post_office_box => import_record[4],
+        :postal_code     => import_record[6],
+        :locality        => import_record[7],
+        :imported_id     => import_record[0]
+      }
+      
+      self.save
+      return self
+    end
+
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+    
+    module ClassMethods
+      def import_all
+        records = FasterCSV.parse(File.new(File.join(RAILS_ROOT, 'data', 'admin.ch', 'insurances.csv')), {:col_sep => ",", :headers => true})
+        Insurance.transaction do
+          for record in records
+            puts Insurance.new.import(record)
+          end
+        end
+      end
+    end
+  end
+end
+
+Insurance.send :include, Insurances::Import
