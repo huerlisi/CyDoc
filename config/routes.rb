31228394d1f04f9dfb0060753e76c5df2a5b7636
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-11-27 22:01:32 +0100

    Drop unused comments from routes; move root definition to top of file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+  # Root
+  map.root :controller => "welcome"
+
   map.filter 'locale'
   # Authentication
   map.logout '/logout', :controller => 'authentication_sessions', :action => 'destroy'
@@ -80,34 +83,6 @@ ActionController::Routing::Routes.draw do |map|
     drug_product.resources :drug_articles, :shallow => true
   end
   
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "welcome"
-
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
