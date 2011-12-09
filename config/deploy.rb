401c6575add99f97efac739574ac6d41f365a279
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-12-09 07:09:54 +0100

    Update capones_recipes.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,11 @@
-require 'capones_recipes/cookbook/rails'
-require 'capones_recipes/tasks/restful_authentication'
-require 'capones_recipes/tasks/carrier_wave'
-
 # Application
 set :application, 'cydoc'
 set :repository,  'git@github.com:huerlisi/CyDoc.git'
+
+require 'capones_recipes/cookbook/rails'
+require 'capones_recipes/tasks/restful_authentication'
+require 'capones_recipes/tasks/carrier_wave'
+require 'capones_recipes/tasks/sync'
 
 # Staging
 set :default_stage, "staging"
@@ -24,25 +25,3 @@ set :use_sudo, false
 set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 set :copy_exclude, [".git", "spec", "test", "stories"]
-
-# Provider
-# ========
-# Load configuration
-config_path = File.expand_path('~/.capones.yml')
-
-if File.exist?(config_path)
-  # Parse config file
-  config = YAML.load_file(config_path)
-
-  # States
-  deploy_target_path = File.expand_path(config['deploy_target_repository']['path'])
-
-  # Add stages
-  set :stage_dir, File.join(deploy_target_path, application, 'stages')
-  load_paths << ""
-end
-
-# Plugins
-# =======
-# Multistaging
-require 'capistrano/ext/multistage'
