ace5d3e568abb08a56ceb7a98ba8900d0781e955
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-31 00:03:02 +0100

    Drop Accounting prefix in vcards object_type for banks.

---
+++ db/migrate/20120130223548_drop_bank_prefix_in_vcards.rb
@@ -0,0 +1,5 @@
+class DropBankPrefixInVcards < ActiveRecord::Migration
+  def self.up
+    Vcard.update_all("object_type = 'Bank'", "object_type = 'Accounting::Bank'")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120104134202) do
+ActiveRecord::Schema.define(:version => 20120130223548) do
 
   create_table "accounts", :force => true do |t|
     t.string   "number"
