af01444a496c46961027e63dfd765e5783ec278e
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-01-31 14:06:49 +0100

    Drop print_reminder_for action.

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :phone_numbers
 
   # Billing
-  map.resources :invoices, :collection => {:print_all => :post, :print_reminders_for_all => :post, :create_treatments => :get}, :member => {:print => :post, :print_reminder_letter => :post, :insurance_recipe => :get, :patient_letter => :get, :reminder => :get, :reactivate => :post} do |invoice|
+  map.resources :invoices, :collection => {:print_all => :post, :create_treatments => :get}, :member => {:print => :post, :print_reminder_letter => :post, :insurance_recipe => :get, :patient_letter => :get, :reminder => :get, :reactivate => :post} do |invoice|
     invoice.resources :bookings
   end
   map.resources :invoice_batch_jobs, :member => {:reprint => :post}
