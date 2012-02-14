e5b0c4c4e0233c2026a59a05c6674cd7d4b63a0c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-02-14 08:06:50 +0100

    New migration instantiate_invoice_vcards.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120203104911) do
+ActiveRecord::Schema.define(:version => 20120213204741) do
 
   create_table "accounts", :force => true do |t|
     t.string   "number"
