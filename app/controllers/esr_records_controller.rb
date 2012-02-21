2203a38745e9be6f67f8ac7efc0c9cdb4e313265
Simon Hürlimann <simon.huerlimann@cyt.ch>
2012-02-21 13:00:43 +0100

    Implement AJAX for esr_record actions.

--- app/controllers/esr_records_controller.rb
+++ app/controllers/esr_records_controller.rb
@@ -1,6 +1,7 @@
 class EsrRecordsController < ApplicationController
   # Inherited Resources
   inherit_resources
+  respond_to :html, :js
 
   # Scopes
   has_scope :by_state
@@ -11,7 +12,9 @@ class EsrRecordsController < ApplicationController
     @esr_record.invoice.write_off("Korrektur nach VESR Zahlung").save
     @esr_record.write_off!
 
-    redirect_to @esr_record.esr_file
+    respond_to do |format|
+      format.html {redirect_to @esr_record.esr_file}
+    end
   end
 
   def book_extra_earning
@@ -19,6 +22,8 @@ class EsrRecordsController < ApplicationController
     @esr_record.invoice.book_extra_earning("Korrektur nach VESR Zahlung").save
     @esr_record.book_extra_earning!
 
-    redirect_to @esr_record.esr_file
+    respond_to do |format|
+      format.html {redirect_to @esr_record.esr_file}
+    end
   end
 end
