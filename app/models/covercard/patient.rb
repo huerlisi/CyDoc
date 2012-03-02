543dccae378cfb1f6a971e327ea9b283fce97387
Roman Simecek <roman.simecek@cyt.ch>
2012-03-02 15:42:40 +0100

    Implemented a mock patient for the covercard integration.

---
+++ app/models/covercard/patient.rb
@@ -0,0 +1,23 @@
+module Covercard
+  class Patient
+    
+    def self.find(value)
+      return nil unless value && value.length == 19
+      
+      vcard = Vcard.new(:family_name => 'Simpson', 
+                        :given_name => 'Homer', 
+                        :street_address => 'Evergreen Terrace 742',
+                        :postal_code => '9373',
+                        :region => 'Springfield')
+      Patient.new(:vcard => vcard, :billing_vcard => vcard, :birth_date => Date.today, :sex => 'm')
+    end
+
+    attr_accessor :vcard, :birth_date, :sex, :only_year_of_birth, :billing_vcard, :vcard
+
+    def initialize(args)
+      args.each do |k,v|
+        instance_variable_set("@#{k}", v) unless v.nil?
+      end
+    end
+  end
+end
