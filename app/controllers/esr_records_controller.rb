085a2f46b4d55d62f723d38538c302ea6a8a6fc0
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-04-19 13:16:10 +0200

    Show flash message if esr correction booking did not work.

--- app/controllers/esr_records_controller.rb
+++ app/controllers/esr_records_controller.rb
@@ -15,21 +15,44 @@ class EsrRecordsController < ApplicationController
   # State events
   def write_off
     @esr_record = EsrRecord.find(params[:id])
-    @esr_record.create_write_off_booking && @esr_record.write_off!
+    if @esr_record.create_write_off_booking
+      @esr_record.write_off!
 
-    respond_to do |format|
-      format.js {}
-      format.html {redirect_to @esr_record.esr_file}
+      respond_to do |format|
+        format.js {}
+        format.html {redirect_to @esr_record.esr_file}
+      end
+    else
+      flash[:error] = 'Konnte Korrekturbuchung nicht erstellen.'
+
+      respond_to do |format|
+        format.js {
+          render 'show_flash_message'
+        }
+        format.html {redirect_to @esr_record.esr_file}
+      end
     end
   end
 
   def book_extra_earning
     @esr_record = EsrRecord.find(params[:id])
-    @esr_record.create_extra_earning_booking && @esr_record.book_extra_earning!
 
-    respond_to do |format|
-      format.js {}
-      format.html {redirect_to @esr_record.esr_file}
+    if @esr_record.create_extra_earning_booking
+      @esr_record.book_extra_earning!
+
+      respond_to do |format|
+        format.js {}
+        format.html {redirect_to @esr_record.esr_file}
+      end
+    else
+      flash[:error] = 'Konnte Korrekturbuchung nicht erstellen.'
+
+      respond_to do |format|
+        format.js {
+          render 'show_flash_message'
+        }
+        format.html {redirect_to @esr_record.esr_file}
+      end
     end
   end
 
