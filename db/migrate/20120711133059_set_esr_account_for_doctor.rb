db2fe2c88d8e9be365f168d25ab4e23ce1482267
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-08-08 16:46:01 +0200

    Make set_esr_account_for_doctor migration robust against users with object not a Doctor.

--- db/migrate/20120711133059_set_esr_account_for_doctor.rb
+++ db/migrate/20120711133059_set_esr_account_for_doctor.rb
@@ -1,6 +1,9 @@
 class SetEsrAccountForDoctor < ActiveRecord::Migration
   def self.up
     User.find_each do |user|
+      # Guard
+      next unless user.object.is_a?(Doctor)
+
       doctor = user.object
       account = doctor.accounts.find(:first, :conditions => {:type => "BankAccount"})
       doctor.esr_account = account
