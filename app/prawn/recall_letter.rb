74b92270346f354c13122bb29709ac02e6a77c22
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-09-12 12:21:35 +0200

    Revert "Refactor recall PDF to use more LetterDocument code."
    This reverts commit ec1a3fda5bd57e8499fe91f21ec075ee71113ee4.

--- app/prawn/recall_letter.rb
+++ app/prawn/recall_letter.rb
@@ -8,25 +8,78 @@ module Prawn
     def to_pdf(recall, params = {})
       doctor = recall.doctor
 
-      letter_header(recall.doctor, recall.patient, "Vorsorge lohnt sich!")
+      # sender address
+      bounding_box([1.7.cm, 25.5.cm], :width => 7.cm) do
+        text [doctor.honorific_prefix, doctor.given_name, doctor.family_name].join(' ')
+        font "Helvetica", :size => 8 do
+          text doctor.vcard.street_address
+          text doctor.vcard.postal_code + " " + doctor.vcard.locality
 
-      text " "
+          contacts = doctor.vcard.contacts
+          labels = contacts.collect{|contact| contact.label}
+          numbers = contacts.collect{|contact| contact.number}
 
-      greeting(recall.patient)
+          bounding_box([0.cm, cursor - 0.2.cm], :width => 2.cm) do
+            bounding_box([0.cm, bounds.top], :width => 0.8.cm) do
+              text labels.join("\n")
+            end
 
-      text "Wie besprochen erlauben wir uns, Sie an die nächste Untersuchung zu erinnern."
-      text "Wir haben für Sie folgenden Terminvorschlag reserviert:"
+            bounding_box([1.cm, bounds.top], :width => 5.cm) do
+              text numbers.join("\n")
+            end
+          end
+        end
+      end
 
-      text " "
+      # Date
+      bounding_box([11.5.cm, 24.cm], :width => 6.cm) do
+        text doctor.vcard.locality + ", " + Date.today.to_s
+      end
 
-      text "#{recall.appointment.date}, #{recall.appointment.from} Uhr", :style => :bold
+      # receiver address
+      bounding_box([11.5.cm, 21.5.cm], :width => 6.cm) do
+        text recall.patient.honorific_prefix
+        text recall.patient.given_name + " " + recall.patient.family_name
+        text recall.patient.vcard.street_address
+        text recall.patient.vcard.postal_code + " " + recall.patient.vcard.locality
+      end
 
-      text " "
+      # Subject
+      bounding_box([1.7.cm, 16.cm], :width => 16.cm) do
+        font "Helvetica", :style => :bold do
+          text "Vorsorge lohnt sich!"
+        end
+      end
 
-      text "Sollte Ihnen der Termin nicht passen, danken wir Ihnen für eine möglichst baldige Meldung. Gerne verabreden wir mit Ihnen dann ein anderes Datum."
+      # Greeting
+      bounding_box([1.7.cm, 14.5.cm], :width => 16.cm) do
+        greeting(recall.patient)
+
+        text "Wie besprochen erlauben wir uns, Sie an die nächste Untersuchung zu erinnern."
+        text "Wir haben für Sie folgenden Terminvorschlag reserviert:"
+
+        text "\n"
+
+        font "Helvetica", :style => :bold do
+          text "#{recall.appointment.date}, #{recall.appointment.from} Uhr"
+        end
+
+        text "\n"
+
+        text "Sollte Ihnen der Termin nicht passen, danken wir Ihnen für eine möglichst baldige Meldung. Gerne verabreden wir mit Ihnen dann ein anderes Datum."
+
+      end
 
       # Salutations
-      common_closing(doctor)
+      bounding_box([11.5.cm, 7.5.cm], :width => 6.cm) do
+        text "Mit freundlichen Grüssen"
+
+        text "\n"
+        text "\n"
+
+        text doctor.honorific_prefix + " " + doctor.family_name
+        text "und Praxisteam"
+      end
 
       render
     end
