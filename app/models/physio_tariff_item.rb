3b320521a5bbac0cfefe1edc970659c1190154a9
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-09-12 11:58:02 +0200

    Whitespace fix for case statement.

--- app/models/physio_tariff_item.rb
+++ app/models/physio_tariff_item.rb
@@ -1,19 +1,19 @@
 class PhysioTariffItem < TariffItem
   def unit_mt
     case reason
-      when "Unfall"
-        1.0
-      else
-        1.03
+    when "Unfall"
+      1.0
+    else
+      1.03
     end
   end
 
   def unit_tt
     case reason
-      when "Unfall"
-        1.0
-      else
-        1.03
+    when "Unfall"
+      1.0
+    else
+      1.03
     end
   end
 
