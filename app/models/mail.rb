a01802ce7c66b6ddfbc8c4a902299a15ab4dc4dc
Andi Staub (CyT) <andi.staub@cyt.ch>
2012-09-27 11:09:55 +0200

    Sent_on is now automaticaly set by the ActionMailer.

--- app/models/mail.rb
+++ app/models/mail.rb
@@ -1,11 +1,10 @@
 class Mail < ActionMailer::Base
 
-  def message_to_zytolabor(sent_at = Time.now)
+  def message_to_zytolabor
     @subject    = 'Mail#message_to_zytolabor'
     @body       = {}
     @recipients = ''
     @from       = ''
-    @sent_on    = sent_at
     @headers    = {}
   end
 end
