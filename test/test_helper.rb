c2abd224adfd2649209d21054f0d3df7d9549083
Andi Staub (CyT) <andi.staub@cyt.ch>
2012-09-27 10:08:58 +0200

    Updated test_helper path.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'test_help'
+require 'test//test_help'
 
 require 'application_controller_mock'
 
@@ -18,7 +18,7 @@ class Test::Unit::TestCase
   # don't care one way or the other, switching from MyISAM to InnoDB tables
   # is recommended.
   #
-  # The only drawback to using transactional fixtures is when you actually 
+  # The only drawback to using transactional fixtures is when you actually
   # need to test transactions.  Since your test is bracketed by a transaction,
   # any transactions started in your code will be automatically rolled back.
   self.use_transactional_fixtures = true
