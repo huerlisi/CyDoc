93704f926d86773a721ed2ff38eb5b547bb647f6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-10-01 15:27:05 +0200

    Ruby 1.9 fixes in Treatment.

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Treatment < ActiveRecord::Base
   # Associations
   has_many :invoices, :dependent => :destroy, :order => 'value_date DESC, created_at DESC'
@@ -78,11 +80,16 @@ class Treatment < ActiveRecord::Base
   # XML Invoices
   def reason_xml
     case reason
-      when 'Unfall': 'accident'
-      when 'Krankheit': 'disease'
-      when 'Mutterschaft': 'maternity'
-      when 'Prävention': 'prevention'
-      when 'Geburtsfehler': 'birthdefect'
+      when 'Unfall'
+        'accident'
+      when 'Krankheit'
+        'disease'
+      when 'Mutterschaft'
+        'maternity'
+      when 'Prävention'
+        'prevention'
+      when 'Geburtsfehler'
+        'birthdefect'
     end
   end
 
