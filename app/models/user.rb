0c3b98cec3476313e4b52ebe92a82a220d879336
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-10-03 08:25:32 +0200

    Drop registerable devise strategy.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :confirmable, :validatable
 
   # CyDoc
