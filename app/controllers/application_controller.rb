7cf83af852c1a77bcf15a630e84300120e524320
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-10-03 08:08:54 +0200

    Drop unused code in application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
-require 'digest/sha2'
 require 'prawn/measurement_extensions'
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '3fcb7aa9a24a90ec6fde37b3755a9ed7'
