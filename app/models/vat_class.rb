5e1cf9457392213fecff3217b7950afc18e292cc
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-10-22 22:31:07 +0200

    Drop shortcuts .full, .reduced, .excluded from VatClass.

--- app/models/vat_class.rb
+++ app/models/vat_class.rb
@@ -10,18 +10,6 @@ class VatClass < ActiveRecord::Base
   end
 
   # Shortcuts
-  def self.full
-    valid.full.first
-  end
-
-  def self.reduced
-    valid.reduced.first
-  end
-
-  def self.excluded
-    valid.excluded.first
-  end
-
   def code_name
     case code
       when 'full'
