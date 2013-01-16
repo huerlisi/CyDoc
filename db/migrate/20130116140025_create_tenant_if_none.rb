745064843c7a8e8450c38263378da62e7fc348aa
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-16 15:17:28 +0100

    New migration to create tenant if there is none.

---
+++ db/migrate/20130116140025_create_tenant_if_none.rb
@@ -0,0 +1,17 @@
+class CreateTenantIfNone < ActiveRecord::Migration
+  def up
+    return unless Tenant.count == 0
+
+    tenant = Tenant.new
+    tenant.users = User.all
+    tenant.save
+
+    Role::NAMES.each do |name|
+      Role.create!(:name => name)
+    end
+
+    User.all.each do |user|
+      user.role_texts = ["sysadmin"]
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130103115455) do
+ActiveRecord::Schema.define(:version => 20130116140025) do
 
   create_table "account_types", :force => true do |t|
     t.string   "name",       :limit => 100
