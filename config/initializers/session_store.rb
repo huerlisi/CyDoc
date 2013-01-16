de2cf2d334c52dc3f710608d423c95806247687c
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-16 14:56:58 +0100

    Bump session_store id.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 
 # Be sure to restart your server when you modify this file.
 
-CyDoc::Application.config.session_store :cookie_store, :key => '_cydoc_'
+CyDoc::Application.config.session_store :cookie_store, :key => '_cydoc2_'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
