8405d1d28b467fed3cb4f5488839b508e2f591f0
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-16 17:17:31 +0100

    Account Type migration.

---
+++ db/migrate/20130116155708_add_account_type_if_none.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+
+class AddAccountTypeIfNone < ActiveRecord::Migration
+  def up
+    return unless AccountType.count == 0
+
+    AccountType.create!([
+      {:name => "current_assets", :title => "Umlaufvermögen"},
+      {:name => "capital_assets", :title => "Anlagevermögen"},
+      {:name => "outside_capital", :title => "Fremdkapital"},
+      {:name => "equity_capital", :title => "Eigenkapital"},
+      {:name => "costs", :title => "Aufwand"},
+      {:name => "earnings", :title => "Ertrag"},
+    ])
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130116140025) do
+ActiveRecord::Schema.define(:version => 20130116155708) do
 
   create_table "account_types", :force => true do |t|
     t.string   "name",       :limit => 100
