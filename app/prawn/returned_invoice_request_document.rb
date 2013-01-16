6ef29c8604229046a3294b698146b6d12472a813
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-16 19:33:46 +0100

    Add encoding hint to ReturnedInvoiceRequestDocument.

--- app/prawn/returned_invoice_request_document.rb
+++ app/prawn/returned_invoice_request_document.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 module Prawn
   class ReturnedInvoiceRequestDocument < Prawn::LetterDocument
     def to_pdf(doctor, params)
@@ -9,7 +11,7 @@ module Prawn
 
       free_text(
         "Rechnungen an folgende Patientinnen konnten von der Post nicht zugestellt werden.
-        
+
         Dürfen wir Sie bitten, die Adressen mit ihrer Patientenkartei zu überprüfen. Wir wären froh, wenn sie die Adressen allenfalls korrigieren und uns dieses Blatt zurückfaxen würden."
       )
       text " "
