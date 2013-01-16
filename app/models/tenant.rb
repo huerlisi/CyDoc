308fbe293d721ec7505095b9e97c7568e2fbaa91
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-01-16 16:51:18 +0100

    Partial port of Tenant.fiscal_year support from Bookyt.

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -10,6 +10,30 @@ class Tenant < ActiveRecord::Base
     doctor.to_s
   end
 
+  # Fiscal Years
+  # ============
+  validates_date :fiscal_year_ends_on
+
+  def fiscal_year_ends_on
+    settings[:fiscal_year_ends_on] || Booking.first.value_date.end_of_year
+  end
+
+  def fiscal_period(year)
+    final_day_of_fiscal_year = Date.new(year, fiscal_year_ends_on.month, fiscal_year_ends_on.day)
+    first_day_of_fiscal_year = final_day_of_fiscal_year.ago(1.year).in(1.day)
+
+    return :from => first_day_of_fiscal_year.to_date, :to => final_day_of_fiscal_year.to_date
+  end
+
+  def fiscal_years
+    first_year = fiscal_year_ends_on.year
+    final_year = Date.today.year + 1
+
+    (first_year..final_year).map{|year|
+      fiscal_period(year)
+    }
+  end
+
   # Attachments
   # ===========
   has_many :attachments, :as => :object
