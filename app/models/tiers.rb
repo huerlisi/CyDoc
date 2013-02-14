969c649b649ab76689811820d0eb707459c4651e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-02-14 16:22:22 +0100

    Validate presence of Tiers.biller and .provider.

--- app/models/tiers.rb
+++ app/models/tiers.rb
@@ -4,9 +4,11 @@ class Tiers < ActiveRecord::Base
 
   belongs_to :biller, :class_name => 'Employee'
   attr_accessible :biller, :biller_id
+  validates :biller, :presence => true
 
   belongs_to :provider, :class_name => 'Employee'
   attr_accessible :provider, :provider_id
+  validates :provider, :presence => true
 
   belongs_to :insurance
   belongs_to :patient
