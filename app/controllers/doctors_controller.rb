1244f99b8591ad27f547acfe66377291143eda45
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-02-15 20:50:34 +0100

    Unify people views with hozr.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,16 +1,4 @@
-# -*- encoding : utf-8 -*-
-class DoctorsController < AuthorizedController
-  # has_many :phone_numbers
-  def new_phone_number
-    if resource_id = params[:id]
-      @doctor = Doctor.find(resource_id)
-    else
-      @doctor = resource_class.new
-    end
-
-    @vcard = @doctor.vcard
-    @item = @vcard.contacts.build
-
-    respond_with @item
-  end
+# encoding: utf-8'
+class DoctorsController < PeopleController
+  defaults :resource_class => Doctor
 end
