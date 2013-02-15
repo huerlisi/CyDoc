b7cf7cc35c79c622b0385b7a68365ebc8df3c1c6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-02-15 23:52:41 +0100

    Drop in_place_edit declarations for treatment.

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -2,11 +2,6 @@
 class TreatmentsController < AuthorizedController
   has_scope :by_state
 
-  in_place_edit_for :session, :date
-  # TODO: is duplicated in ServiceRecordsController
-  in_place_edit_for :service_record, :ref_code
-  in_place_edit_for :service_record, :quantity
-
   # GET /treatments/1
   # GET /patients/1/treatments/2
   def show
