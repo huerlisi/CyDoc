971643ea571cdffd566a24b74ac2724d8b1984df
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2013-07-15 10:54:19 +0200

    Implement simple bookings for invoices.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130116155708) do
+ActiveRecord::Schema.define(:version => 20130707221608) do
 
   create_table "account_types", :force => true do |t|
     t.string   "name",       :limit => 100
