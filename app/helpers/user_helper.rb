c14fb5e37349a2ce9f79675e132bebc56dc3d3c3
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-01-06 16:54:49 +0100

    Show all persons as possible objects in the user relation form field.

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,7 +3,7 @@ module UserHelper
   def user_object_collection
     [
       [t_model(Doctor), Doctor.all.map{|u| [u.to_s, "Doctor:#{u.id}"]}].compact,
-      [t_model(Employee), Employee.all.map{|u| [u.to_s, "Person:#{u.id}"]}].compact
+      [t_model(Person), Person.all.map{|u| [u.to_s, "Person:#{u.id}"]}].compact
     ].compact
   end
 end
