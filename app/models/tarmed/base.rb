2db94addc1bd7186e1758a265eac10079ca53ffe
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-10-03 00:23:59 +0200

    Drop dead code in tarmed import model.

--- app/models/tarmed/base.rb
+++ app/models/tarmed/base.rb
@@ -35,75 +35,21 @@ class Tarmed::Base < ActiveRecord::Base
     end
   end
 
-  def find_int
-    self.class.find_int(@int_record.attributes[self.class.int_id])
-  end
-
   def assign
-    if @to_delete
-      delete
-    else
-      @int_record = create_or_update
-      @int_record.save!
-    end
-  end
-
-  def delete
-    int_record = find_int
-
-    if int_record
-      puts "Deleting #{int_record}"
-      int_record.destroy
-      puts
-    else
-      puts "Already deleted #{@int_record}" if @@log == :debug
-      puts if @@log == :debug
-    end
+    @int_record = create_or_update
+    @int_record.save!
   end
 
   def create_or_update
-    int_record = find_int
-
-    if int_record
-      int_record.attributes = int_record.attributes.merge(@int_record.attributes.reject{|key, value| key == 'updated_at' or key == 'created_at' or key == 'id'})
-      unless int_record.changes.empty? or @@log == :debug
-        puts "Updating #{int_record}..."
-        changes = int_record.changes.map{|column, values| "  #{column}: #{values[0]} => #{values[1]}"}.join("\n")
-        puts changes
-      end
-
-      create_or_update_associations(int_record) if self.class.method_defined? :create_or_update_associations
-
-    else
-      puts "Adding #{@int_record}..."
-      int_record = @int_record
-      changes = int_record.changes.map{|column, values| "  #{column}: #{values[0]} => #{values[1]}"}.join("\n")
-      puts changes if @@log == :debug
-    end
+    puts "Adding #{@int_record}..."
+    int_record = @int_record
+    changes = int_record.changes.map{|column, values| "  #{column}: #{values[0]} => #{values[1]}"}.join("\n")
+    puts changes if @@log == :debug
 
     puts unless int_record.changes.empty?
 
     return int_record
   end
-
-  def import
-    @int_record = self.class.int_class.new
-
-      begin
-        @int_record.code = self.code
-        @int_record.amount_mt = self.amount_mt
-        @int_record.amount_tt = self.amount_tt
-        @int_record.remark = self.name
-
-        print "ID: #{self.id} OK\n"
-      rescue Exception => ex
-        print "ID: #{self.id} => #{ex.message}\n\n"
-        return
-      end
-
-    assign
-  end
-
 
   def self.import_all(do_clean)
     TarmedTariffItem.delete_all if do_clean
