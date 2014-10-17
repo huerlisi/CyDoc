67edd804b36c0977945fb211fc77b367871882be
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-10-17 16:24:05 +0200

    Create initial tenant, user and doctor in database seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,34 @@ Role::NAMES.each do |name|
   Role.create!(:name => name)
 end
 
+tenant =
+  Tenant.create!(
+    :person => Person.new(
+      :vcard => Vcard.new(:full_name => 'Demo Tenant')
+    )
+  )
+
+doctor = Doctor.create!(
+  :vcard => Vcard.new(
+    :honorific_prefix => 'Frau Dr. med.',
+    :family_name => "Muster",
+    :given_name => "Melanie",
+    :street_address => "Zentralgasse 99",
+    :postal_code => "6300",
+    :locality => "Zug"
+  )
+)
+
+user = User.create!(
+  :login => "doctor",
+  :password => "doctor1234",
+  :password_confirmation => "doctor1234",
+  :email => "cydoc@example.com",
+  :tenant => tenant,
+  :object => doctor,
+  :role_texts => ['sysadmin']
+)
+
 # Account Types
 # =============
 current_assets, capital_assets, outside_capital, equity_capital, costs, earnings =
