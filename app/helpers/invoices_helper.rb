b07e2e5ebe6f2c04c99ee5a44e62ac588f3eaea6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-11-14 16:06:15 +0100

    Simplify invoices helper code.

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -5,32 +5,23 @@ module InvoicesHelper
   end
 
   def tax_points_mt(invoice, tariff_type)
-    amount = invoice.amount_mt(tariff_type)
-    if amount == 0.0
-      return nil
-    else
-      tax_points = invoice.tax_points_mt(tariff_type)
-      return "(" + sprintf("%0.2f", tax_points) + ")"
-    end
+    return nil if invoice.amount_mt(tariff_type) == 0.0
+
+    tax_points = invoice.tax_points_mt(tariff_type)
+    "(" + sprintf("%0.2f", tax_points) + ")"
   end
 
   def tax_points_tt(invoice, tariff_type)
-    amount = invoice.amount_tt(tariff_type)
-    if amount == 0.0
-      return nil
-    else
-      tax_points = invoice.tax_points_tt(tariff_type)
-      return "(" + sprintf("%0.2f", tax_points) + ")"
-    end
+    return nil if invoice.amount_tt(tariff_type) == 0.0
+
+    tax_points = invoice.tax_points_tt(tariff_type)
+    "(" + sprintf("%0.2f", tax_points) + ")"
   end
 
   def tax_points(invoice, tariff_type)
-    amount = invoice.amount(tariff_type)
-    if amount == 0.0
-      return nil
-    else
-      tax_points = invoice.tax_points(tariff_type)
-      return "(" + sprintf("%0.2f", tax_points) + ")"
-    end
+    return nil if invoice.amount(tariff_type) == 0.0
+
+    tax_points = invoice.tax_points(tariff_type)
+    "(" + sprintf("%0.2f", tax_points) + ")"
   end
 end
