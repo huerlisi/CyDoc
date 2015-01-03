4665230363742511eeeaa2bba3ac65592a120142
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-01-03 01:01:53 +0100

    Use self. for table_name and primary_key in Models.

--- app/models/tarmed/leistung_hierarchie.rb
+++ app/models/tarmed/leistung_hierarchie.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Tarmed::LeistungHierarchie < Tarmed::Base
-  set_table_name "LEISTUNG_HIERARCHIE"
+  self.table_name = "LEISTUNG_HIERARCHIE"
 
   belongs_to :master, :class_name => 'Leistung', :foreign_key => 'LNR_MASTER'
   belongs_to :slave, :class_name => 'Leistung', :foreign_key => 'LNR_SLAVE'
