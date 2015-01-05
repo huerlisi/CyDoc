1697159bfabe991e782a9ef62809a8b27622bd39
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-01-05 22:12:45 +0100

    Drop all tests.
    No sensinble test suite exists. Need to start from zero anyway.

--- test/unit/helpers/drug_articles_helper_test.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-require 'test_helper'
-
-class DrugArticlesHelperTest < ActionView::TestCase
-end
--- test/unit/session_test.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-require 'test_helper'
-
-class SessionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
